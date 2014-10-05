704d4d270875d2a1e2e4fe153115f83df0f56cd9
Parham Alvani <parham.alvani@gmail.com>
2014-10-05 22:33:33 +0330

    nothing projcet improved.

--- Assembly/nothing/nothing.c
+++ Assembly/nothing/nothing.c
@@ -1,8 +1,11 @@
 #include <string.h>
+#include <stdio.h>
 
-void hello_writer(const char*, int);
+int hello_writer(const char*, int);
 
 int main(int argc, char* argv[]){
 	char* TEST = "This is a test\nBut i love it !!!\nWTF ?? it's assembly ?!?!\n";
-	hello_writer(TEST, strlen(TEST));
+	int ret;
+	ret = hello_writer(TEST, strlen(TEST));
+	printf("Return: %d\n", ret);
 }
