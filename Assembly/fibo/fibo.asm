8e9f8d3334c734771d65b11e8a3a28245e7d85e5
Parham Alvani <parham.alvani@gmial.com>
2014-10-09 06:56:25 +0330

    fibo project is completed. sanbox project is added.

--- Assembly/fibo/fibo.asm
+++ Assembly/fibo/fibo.asm
@@ -1,11 +1,41 @@
-section .data
+FIN_N equ 10
 
-NUMBER:		equ 10
-FIB_EVEN:	dd 1
-FIB_ODD:	dd 1
+section .data
+	number: db 1
+	fib_even: dd 1
+	fib_odd: dd 1
 
 section .text
-	mov ecx, 0
-	mov eax, [FIN_EVEN]
-	mov ebx, [FIN_ODD]
-
+	global main
+main:
+	mov eax, 0
+	mov edx, 0
+	mov al, [number]
+	inc al
+	mov byte [number], al
+	mov bl, 2
+	div bl
+	test ah, ah
+	jz DO_EVEN
+DO_ODD:
+	mov eax, [fib_even]
+	mov ebx, [fib_odd]
+	add ebx, eax
+	mov dword [fib_odd], ebx
+	mov al, [number]
+	cmp al, FIN_N
+	jz FINISH
+	jmp main
+DO_EVEN:
+	mov eax, [fib_even]
+	mov ebx, [fib_odd]
+	add eax, ebx
+	mov dword [fib_even], eax
+	mov al, [number]
+	cmp al, FIN_N
+	jz FINISH
+	jmp main
+FINISH:
+	mov eax, 1
+	mov ebx, 0
+	int 80H
