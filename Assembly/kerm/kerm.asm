a0917a97759c2ddc09d13f12b6d193471112666d
Parham Alvani <parham.alvani@gmail.com>
2014-10-17 02:31:09 +0330

    kerm project improved !!! really ??
    Former-commit-id: 025bb14d689da8dcdc90ed746e18437836bc56ba

--- Assembly/kerm/kerm.asm
+++ Assembly/kerm/kerm.asm
@@ -1,13 +1,23 @@
+section .data
+	A: db 1
 section .text
-	global main
-main:
+	global _start
+_start:
+	jmp main
+	
+	times 1000 * 1000 * 10 db 1
+
+main:	
 	mov eax, cs
+	
 	; mov ds, eax
 	; sysenter
 	; in ax, 64h
+	
 	mov r8, 1
 	mov r9, 10
 	mov r10, 11
+	
 	mov eax, 1
 	mov ebx, 0
 	int 80h
