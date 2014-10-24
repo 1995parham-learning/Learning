f3debbbcecaa19f61d2faa609aba03125401f475
Parham Alvani <parham.alvani@gmail.com>
2014-10-24 22:40:21 +0330

    pthread-scheduling project is added.
    Former-commit-id: 946d2fd70a3f72ad7d0c2ff1edc74e0a7b65abb4

---
+++ C++/pthread-scheduling/pthread-scheduling.c
@@ -0,0 +1,35 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+	pthread_attr_t attr;
+	int scope;
+	int policy;
+	pthread_attr_init(&attr);
+
+	if(pthread_attr_getscope(&attr, &scope) != 0){
+		perror("pthread_attr_getscope()");
+	}
+	switch(scope){
+		case PTHREAD_SCOPE_PROCESS:
+			printf("PTHREAD_SCOPE_PROCESS\n");
+			break;
+		case PTHREAD_SCOPE_SYSTEM:
+			printf("PTHREAD_SCOPE_SYSTEM\n");
+	}
+	
+	if(pthread_attr_getschedpolicy(&attr, &policy) != 0){
+		perror("pthread_attr_getschedpolicy()");
+	}
+	switch(policy){
+		case SCHED_FIFO:
+			printf("SCHED_FIFO\n");
+			break;
+		case SCHED_RR:
+			printf("SCHED_RR\n");
+			break;
+		case SCHED_OTHER:
+			printf("SCHED_OTHER\n");
+			break;
+	}
+}
