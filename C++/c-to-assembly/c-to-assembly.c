2404936f074156415ef4a9c96b16a393442ddf7e
Parham Alvani <parham.alvani@gmial.com>
2014-11-03 02:01:33 +0330

    c-to-assembly project is improved.
    Former-commit-id: 5981ad2891ae0f24bc7a05e19a2140fb4b7cb522

--- C++/c-to-assembly/c-to-assembly.c
+++ C++/c-to-assembly/c-to-assembly.c
@@ -1,9 +1,31 @@
+#include <stdio.h>
+
+struct test_struct{
+	int a;
+	int b;
+};
+
 int f(int a, int b, int c);
 
+int A = 10;
+
 int main(long argc){
+	struct test_struct s;
+	s.a = 10;
+	s.b = 20;
+
+	A += 20;
+
+	printf("%p\n", &A);
+
+	int i = 0;
+	for(i = 0; i < 10; i++){
+	}
+
 	f(1, 2, 3);
 }
 
 int f(int a, int b, int c){
+	A += 30;
 	return a * b * c;
 }
