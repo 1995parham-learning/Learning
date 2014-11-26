6593b5c7a82682ca2002fda957a7313823f049cb
Parham Alvani <parham.alvani@gmial.com>
2014-11-26 07:17:26 +0330

    Haskell project build is added.
    Former-commit-id: bf62c87373de1ce002314b0cdb20ea3ade573492

--- Haskell/Test/Test/A.hs
+++ Haskell/Test/Test/A.hs
@@ -2,3 +2,7 @@ module Test.A where
 
 firstOfEmpty :: [[Char]] -> [Char]
 firstOfEmpty lst = if not (null lst) then head lst else "empty"
+
+lst1 +++ lst2 = if null lst1
+		then lst2
+		else (head lst1) : (tail lst1 +++ lst2)
