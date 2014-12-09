a63df79f8a6b086b4e2ff7b1cac5566daf01103e
Parham Alvani <parham.alvani@gmial.com>
2014-12-09 21:11:27 +0330

    floopy image converted to FAT16

--- C++/FAT/main.c
+++ C++/FAT/main.c
@@ -4,7 +4,7 @@
 // 
 // * Creation Date : 08-12-2014
 //
-// * Last Modified : Mon 08 Dec 2014 02:45:34 AM IRST
+// * Last Modified : Tue 09 Dec 2014 09:08:38 PM IRST
 //
 // * Created By : Parham Alvani (parham.alvani@gmail.com)
 // =======================================
@@ -15,17 +15,19 @@
 #include "FAT.h"
 
 int main(int argc, char* argv[]){
-	int fd = open("/dev/loop1", O_RDONLY); 
+	int fd = open("/dev/loop0", O_RDONLY); 
 	
 	fat_BS_t fat;
 	read(fd, &fat, 512);
 	printf("Boot Jump: %2X %2X %2X\n", fat.bootjmp[0], fat.bootjmp[1], fat.bootjmp[2]);
+	printf("OEM Media: %8s\n", fat.oem_name);
 	printf("Media Type: %2X\n", fat.media_type);
 	printf("Sectors Per Track: %hu\n", fat.sectors_per_track);
 	printf("Sectors Per Cluster: %hu\n", fat.sectors_per_cluster);
 	printf("Reserved Sectors: %hu\n", fat.reserved_sector_count);
 	printf("FAT Tables: %hhu\n", fat.table_count); 
 	printf("Table Size: %hu\n", fat.table_size_16);
+	printf("Volume Label: %11s\n", fat.extBS.volume_label);
 
 	fat_addr_t root_cluster = first_data_sector(&fat);
 
