dd7283092fd8997ab0b8f94fd47824508adbfa17
Parham Alvani <parham.alvani@gmial.com>
2014-12-09 21:34:25 +0330

    Finally some rational result is achieved.

--- C++/FAT/FAT.c
+++ C++/FAT/FAT.c
@@ -4,14 +4,14 @@
 // 
 // * Creation Date : 08-12-2014
 //
-// * Last Modified : Tue 09 Dec 2014 08:23:44 PM IRST
+// * Last Modified : Tue 09 Dec 2014 09:28:35 PM IRST
 //
 // * Created By : Parham Alvani (parham.alvani@gmail.com)
 // =======================================
 #include "FAT.h"
 
 fat_addr_t root_dir_sectors(const fat_BS_t* fat_boot){ 
-	return ((fat_boot->root_entry_count * 16) + (fat_boot->bytes_per_sector - 1)) / fat_boot->bytes_per_sector;
+	return ((fat_boot->root_entry_count * 32) + (fat_boot->bytes_per_sector - 1)) / fat_boot->bytes_per_sector;
 }
 fat_addr_t first_fat_sector(const fat_BS_t* fat_boot){
 	return  fat_boot->reserved_sector_count;
@@ -23,5 +23,5 @@ fat_addr_t data_sectors(const fat_BS_t* fat_boot){
 	return fat_boot->total_sectors_16 - (fat_boot->reserved_sector_count + (fat_boot->table_count * fat_boot->table_size_16) + root_dir_sectors(fat_boot));
 }
 fat_addr_t total_clusters(const fat_BS_t* fat_boot){
-	return data_sectors(fat_boot) / fat_boot->sectors_per_cluster;
+	return (data_sectors(fat_boot) / fat_boot->sectors_per_cluster);
 }
