f201ad83e467687b38dce74deedcaa578db9f76a
Parham Alvani <parham.alvani@gmial.com>
2014-12-21 22:20:43 +0330

    unique_ptr project started.

---
+++ C++/unique_ptr/TestClass.h
@@ -0,0 +1,20 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : TestClass.h
+ *
+ * [] Creation Date : 21-12-2014
+ *
+ * [] Last Modified : Sun 21 Dec 2014 10:20:33 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+class TestClass {
+	public:
+		TestClass() = default;
+		TestClass(int number);
+
+	private:
+		int number;
+};
