a1f22837e768a3db4c07a53a528a9186c2f7a45c
Parham Alvani <parham.alvani@gmial.com>
2015-01-04 10:39:28 +0330

    erf project is added.

---
+++ C++/erf/erf.c
@@ -0,0 +1,20 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : erf.c
+ *
+ * [] Creation Date : 04-01-2015
+ *
+ * [] Last Modified : Sun 04 Jan 2015 10:29:13 AM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <math.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	printf("The integral of Normal(0, 1) distribution"
+			"bettween -1.96 and 1.96 is %g\n", erf(1.96 * sqrt(1/2.)));
+}
