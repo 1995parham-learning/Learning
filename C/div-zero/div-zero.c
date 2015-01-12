862809cfc6b40346960cb4ff3d2972c954e95a6b
Parham Alvani <parham.alvani@gmail.com>
2015-01-12 17:41:43 +0330

    Reformatting all codes part #1.
    Former-commit-id: a0da3c15777a06469482b40353827715db3654bf

---
+++ C/div-zero/div-zero.c
@@ -0,0 +1,18 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : div-zero.c
+ *
+ * [] Creation Date : 12-01-2015
+ *
+ * [] Last Modified : Mon 12 Jan 2015 03:29:49 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+int main(int argc, char* argv[])
+{
+	int a = 0;
+	int b = 10;
+	int c = b / a;
+}
---
+++ C/shared-memory/SharedMemoryProducer.c
@@ -0,0 +1,27 @@
+/* 
+ * In The Name Of God
+ * ========================================
+ * [] File Name : SharedMemoryProducer.c
+ *
+ * [] Creation Date : 21-12-2014
+ *
+ * [] Last Modified : Mon 12 Jan 2015 05:17:43 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+const size_t SIZE = 4096;
+
+int main(int argc,char *argv[])
+{
+	int fd = shm_open("firstSharedMemory", O_CREAT | O_RDWR, 0644);
+	void *ptr = mmap(NULL, SIZE, PROT_WRITE, MAP_SHARED, fd, 0);
+	sprintf((char*) ptr, "Hello world of linux IPC\n");
+	printf("I write !!!!\n");
+}
