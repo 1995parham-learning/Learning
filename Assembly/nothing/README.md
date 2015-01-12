64c1200f09653b880136792c40f202e215340f0f
Parham Alvani <parham.alvani@gmail.com>
2015-01-12 21:58:03 +0330

    Reformatting all codes part #5.
    Former-commit-id: da2bcffe27fcf204255e8a7a51d8523ea13c1ddf

---
+++ Assembly/nothing/README.md
@@ -0,0 +1,5 @@
+nothing
+=====
+
+In this project i connect c program with assembly program
+using amd64 calling conversion.
--- Assembly/nothing/nothing-c.c
+++ Assembly/nothing/nothing-c.c
@@ -1,24 +1,23 @@
 /* 
  * In The Name Of God
  * ========================================
- * [] File Name : nothing.c
+ * [] File Name : nothing-c.c
  *
  * [] Creation Date : 21-12-2014
  *
- * [] Last Modified : Sun 21 Dec 2014 01:59:44 AM IRST
+ * [] Last Modified : Mon 12 Jan 2015 09:45:34 PM IRST
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
 */
-#define _GNU_SOURCE
-
 #include <string.h>
 #include <stdio.h>
 
-int hello_writer(const char*, int) __attribute__((stdcall));
+int hello_writer(const char *, int);
 
-int main(int argc, char* argv[]){
-	char* TEST = "This is a test\nBut i love it !!!\nWTF ?? it's assembly ?!?!\n";
+int main(int argc, char *argv[])
+{
+	char *TEST = "This is a test\nBut i love it !!!\nWTF ?? it's assembly ?!?!\n";
 	int ret;
 	ret = hello_writer(TEST, strlen(TEST));
 	printf("Return: %d\n", ret);
