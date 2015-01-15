103d27eb8baea76dc4b96cee5ad026f9ac6b8c96
Parham Alvani <parham.alvani@gmail.com>
2015-01-15 08:51:25 +0330

    Makproject.sh, Renamer.sh is added.
    Former-commit-id: 70ed6e8abba9a6db626ad33abe95f74fa132d95b

---
+++ Shell/MakeProject.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Use this script to create simple project
+# template folder
+
+while [ "$#" -ge "1" ]; do
+	mkdir $1
+	echo $1 > $1/.gitignore
+	vim -c "wq" $1/Makefile
+	echo $1 > $1/README.md
+	echo "=====" >> $1/README.md
+	shift
+done
