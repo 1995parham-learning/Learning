44587416ae70ad36cb901cce3d24916242b79670
Parham Alvani <parham.alvani@gmial.com>
2015-01-15 10:40:11 +0330

    Header added into scripts.
    Former-commit-id: ebb907ad39be07963bcbad1919e3de8e8575a5f1

--- Shell/lsdisk.bash
+++ Shell/lsdisk.bash
@@ -1,4 +1,15 @@
 #!/bin/bash
+#
+# In The Name Of God
+# ========================================
+# [] File Name : lsdisk.bash
+#
+# [] Creation Date : 15-01-2015
+#
+# [] Last Modified : Thu 15 Jan 2015 10:36:33 AM IRST
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
 
 shopt -s extglob
 ls /dev/sd+([[:lower:]])
