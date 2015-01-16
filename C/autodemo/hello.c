4d8e2ccfd8805ec74bd342a957fa6da9bdee4998
Parham Alvani <parham.alvani@gmial.com>
2015-01-16 23:59:36 +0330

    autodemo projec is added.

---
+++ C/autodemo/hello.c
@@ -0,0 +1,18 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : hello.c
+ *
+ * [] Creation Date : 16-01-2015
+ *
+ * [] Last Modified : Fri 16 Jan 2015 11:34:08 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Hi\n");
+}
