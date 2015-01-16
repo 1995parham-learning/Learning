c8718c9004f538d0aaa7d7a7c359d39bbc622b64
Parham Alvani <parham.alvani@gmial.com>
2015-01-16 18:29:08 +0330

    IO project is added.
    Former-commit-id: b20ac3216795ca8d63b9df2b01abc417206fe5d5

---
+++ C/IO/IO.c
@@ -0,0 +1,25 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : IO.c
+ *
+ * [] Creation Date : 16-01-2015
+ *
+ * [] Last Modified : Fri 16 Jan 2015 06:29:03 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	/*
+	 * change it to char *c and test .... HAVE FUN ;-)))
+	*/
+	char c[10];
+
+	scanf("%[1]", c);
+	printf("%s\n", c);
+}
