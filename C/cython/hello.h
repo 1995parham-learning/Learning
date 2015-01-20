41e8b888e3dfa1b94b6e7d0bc6c8a42fb427f96a
Parham Alvani <parham.alvani@gmial.com>
2015-01-20 12:38:47 +0330

    Cython project is added.
    Former-commit-id: 617188438b471db44048bbe3c9bd4af639732365

---
+++ C/cython/hello.h
@@ -0,0 +1,14 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : hello.h
+ *
+ * [] Creation Date : 20-01-2015
+ *
+ * [] Last Modified : Tue 20 Jan 2015 09:46:56 AM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+
+int hello(const char *name);
