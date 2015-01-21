a340899a077e8d0aec478e24134f24f16b17320e
Parham Alvani <parham.alvani@gmail.com>
2015-01-21 09:19:19 +0330

    inline added into c-to-assembly project.
    Former-commit-id: cfe539e73ca2244976e691b179b7e267ff4cea7a

---
+++ C/c-to-assembly/inline.c
@@ -0,0 +1,25 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : inline.c
+ *
+ * [] Creation Date : 21-01-2015
+ *
+ * [] Last Modified : Wed Jan 21 09:18:08 2015
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int a = 100;
+
+	printf("%d\n", a);
+	__asm__("xorl	%1, %1\n"
+		: "=r"(a)
+		: "r"(a)
+		);
+	printf("%d\n", a);
+}
