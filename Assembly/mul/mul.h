199e86806ce229e7426ff85db751a7287dab2b17
Parham Alvani <parham.alvani@gmial.com>
2015-01-23 13:03:50 +0330

    mul project improved.

---
+++ Assembly/mul/mul.h
@@ -0,0 +1,14 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : mul.h
+ *
+ * [] Creation Date : 23-01-2015
+ *
+ * [] Last Modified : Fri 23 Jan 2015 12:02:57 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+
+long long shift_mul(long long n1, long long n2);
