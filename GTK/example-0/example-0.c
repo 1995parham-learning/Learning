f7c64f0121b1fa2f3045cd8fd63d1de2c8934b4c
Parham Alvani <parham.alvani@gmial.com>
2015-02-03 02:13:55 +0330

    GTK Training hub is created. example-0 is added.

---
+++ GTK/example-0/example-0.c
@@ -0,0 +1,49 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : example-0.c
+ *
+ * [] Creation Date : 03-02-2015
+ *
+ * [] Last Modified : Tue 03 Feb 2015 02:13:41 AM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <gtk/gtk.h>
+
+/*
+ * https://developer.gnome.org/gtk3/stable/gtk-getting-started.html
+ * Basics: example-0
+*/
+
+int main(int argc, char *argv[])
+{
+	GtkWidget *window;
+
+	gtk_init(&argc, &argv);
+
+	/*
+	 * The type of the window is GTK_WINDOW_TOPLEVEL, which means that
+	 * the GtkWindow will be managed by the windowing system: it will
+	 * have a frame, a title bar and window controls,
+	 * depending on the platform.
+	*/
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window), "Parham Window");
+
+	/*
+	 * In order to terminate the application when the GtkWindow is destroyed,
+	 * we connect the “destroy” signal to the gtk_main_quit() function.
+	 * This function will terminate the GTK+ main loop started by calling gtk_main()
+	 * later. The “destroy” signal is emitted when a widget is destroyed, either by
+	 * explicitly calling gtk_widget_destroy() or when the widget is unparented.
+	 * Top-level GtkWindows are also destroyed when the Close window control
+	 * button is clicked.
+	*/
+	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+	gtk_widget_show(window);
+
+	gtk_main();
+}
