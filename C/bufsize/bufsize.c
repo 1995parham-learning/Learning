9067e3b74f2fb96059ef0d8255d72a8878138939
Parham Alvani <parham.alvani@gmail.com>
2015-02-04 03:44:16 +0330

    bufsize project is added.
    Former-commit-id: ac5f862539a05163b2572133b7f43a783f330f3a

---
+++ C/bufsize/bufsize.c
@@ -0,0 +1,32 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : bufsize.c
+ *
+ * [] Creation Date : 04-02-2015
+ *
+ * [] Last Modified : Wed 04 Feb 2015 03:41:26 AM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <stdio.h>
+#include <stdio_ext.h>
+
+int main(int argc, char *argv[])
+{
+	FILE *f;
+	int bufsize;
+
+	f = fopen("test.txt","wb");
+	if (f == NULL) {
+		perror("fopen failed\n");
+		return -1;
+	}
+
+	bufsize = __fbufsize(f);
+
+	printf("The buffer size is %d\n",bufsize);
+
+	return 0;
+}
