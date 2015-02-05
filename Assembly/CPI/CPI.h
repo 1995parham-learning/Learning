baf6ae1f46e9d6ad179357bbaf3444665d8c8bac
Parham Alvani <parham.alvani@gmial.com>
2015-02-05 22:38:53 +0330

    CPI project is added.
    Former-commit-id: ec7b88ff57436b58f23ec88f6f080e3cbe5de431

---
+++ Assembly/CPI/CPI.h
@@ -0,0 +1,16 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : CPI.h
+ *
+ * [] Creation Date : 05-02-2015
+ *
+ * [] Last Modified : Thu 05 Feb 2015 10:26:52 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+
+#define PI getPI()
+
+double getPI(void);
