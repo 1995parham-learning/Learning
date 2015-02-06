d608bda14d7aeb7aed444d55a42e6c759ec59305
Parham Alvani <parham.alvani@gmial.com>
2015-02-06 15:39:16 +0330

    symboling project improved.

--- C/symboling/symboling.c
+++ C/symboling/symboling.c
@@ -5,15 +5,20 @@
  *
  * [] Creation Date : 06-02-2015
  *
- * [] Last Modified : Fri 06 Feb 2015 10:47:58 AM IRST
+ * [] Last Modified : Fri 06 Feb 2015 12:07:35 PM IRST
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
 */
 #include <stdio.h>
 
+extern char p;
+
 int main(int argc, char *argv[])
 {
+	printf("%p\n", &p);
+	
+
 	FILE *fout = stdout;
 	fprintf(fout, "Hello world of symboling\n");
 }
