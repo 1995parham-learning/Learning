09586b8645adaf508dbca3bc7126d5549d79e20b
Parham Alvani <parham.alvani@gmial.com>
2015-02-06 11:19:47 +0330

    symboling project is added.

---
+++ C/symboling/symboling.c
@@ -0,0 +1,19 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : symboling.c
+ *
+ * [] Creation Date : 06-02-2015
+ *
+ * [] Last Modified : Fri 06 Feb 2015 10:47:58 AM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	FILE *fout = stdout;
+	fprintf(fout, "Hello world of symboling\n");
+}
