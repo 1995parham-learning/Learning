7bde1f0f87aadb3c30094e4d9bf7f193830cbb4a
Parham Alvani <parham.alvani@gmail.com>
2015-02-08 11:16:08 +0330

    IO project improved.
    Former-commit-id: 42fbfeeaf4ab5031ef4e7fdbff38746076843021

--- C/IO/IO.c
+++ C/IO/IO.c
@@ -5,7 +5,7 @@
  *
  * [] Creation Date : 16-01-2015
  *
- * [] Last Modified : Fri Jan 16 19:00:37 2015
+ * [] Last Modified : Sun 08 Feb 2015 11:15:16 AM IRST
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
@@ -21,7 +21,13 @@ int main(int argc, char *argv[])
 	 * every things goes wrong.
 	*/
 	char c[10];
+	c[0] = '\0';
+	
+	char str[1024];
 
 	scanf("%[1]", c);
 	printf("%s\n", c);
+
+	scanf("%[^:]", str);
+	printf("%s\n", str);
 }
