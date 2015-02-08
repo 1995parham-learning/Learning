21c2f4db42be221a2597438dc1d6e681dd0bb1f2
Parham Alvani <parham.alvani@gmial.com>
2015-02-08 23:27:41 +0330

    typeof project is added.
    Former-commit-id: 3146770910dae12486a9d3ca3ce2a083a5649e2b

---
+++ C/typeof/typeof.c
@@ -0,0 +1,18 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : typeof.c
+ *
+ * [] Creation Date : 08-02-2015
+ *
+ * [] Last Modified : Sun 08 Feb 2015 11:26:09 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+int main(int argc, char *argv[])
+{
+	int A = 10;
+	typeof(A)* a;
+	a = &A;
+}
