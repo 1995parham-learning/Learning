09e3264e503f239383242c13a2438a957459c1fa
Parham Alvani <parham.alvani@gmial.com>
2015-02-08 23:58:12 +0330

    c-to-assembly project improved.

---
+++ C/c-to-assembly/attributes.c
@@ -0,0 +1,21 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : attributes.c
+ *
+ * [] Creation Date : 08-02-2015
+ *
+ * [] Last Modified : Sun 08 Feb 2015 11:55:22 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <stdio.h>
+
+/* Variables Attribute */
+int parham __attribute__((section ("PARHAM"))) = 137303;
+
+int main(int argc, char *argv[])
+{
+	printf("Hello world of attributes... %d\n", parham);
+}
