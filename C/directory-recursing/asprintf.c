4a75934a790e96cb0d24e2b2e8a612a6df0a6b01
Parham Alvani <parham.alvani@gmial.com>
2015-02-15 01:54:10 +0330

    directory-recursing project is added.
    Former-commit-id: abb792f54026a30f8f996d787d7c840034b4fa93

---
+++ C/directory-recursing/asprintf.c
@@ -0,0 +1,49 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : asprintf.c
+ *
+ * [] Creation Date : 01-02-2015
+ *
+ * [] Last Modified : Sun 15 Feb 2015 01:52:54 AM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#include "asprintf.h"
+
+int asprintf(char **str, const char *fmt, ...)
+{
+	va_list argp;
+	int len = 0;
+	/* temp array just for finding size */
+	char one_char[1];
+
+	/* finding size */
+	va_start(argp, fmt);
+	len = vsnprintf(one_char, 1, fmt, argp);
+	if (len < 1) {
+		fprintf(stderr, "An encoding error occured.");
+		fprintf(stderr, "Setting the input pointer to NULL.\n");
+		*str = NULL;
+		return len;
+	}
+	va_end(argp);
+
+	/* allocating space for string */
+	*str = malloc(len + 1);
+
+	/* printing string to allocated space */
+	if (!str) {
+		fprintf(stderr, "Couldn't allocate %i bytes.\n", len + 1);
+		return -1;
+	}
+	va_start(argp, fmt);
+	vsnprintf(*str, len + 1, fmt, argp);
+	va_end(argp);
+	return len;
+}
---
+++ C/directory-recursing/main.c
@@ -0,0 +1,41 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : main.c
+ *
+ * [] Creation Date : 15-02-2015
+ *
+ * [] Last Modified : Sun 15 Feb 2015 01:54:05 AM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <stdio.h>
+
+#include "process_dir.h"
+
+void print_dir(struct filestr in)
+{
+	for (int i = 0; i < in.depth - 1; i++)
+		printf("    ");
+	printf("|- %s\n", in.name);
+	for (int i = 0; i < in.depth -1; i++)
+		printf("    ");
+	printf("|---|\n");
+}
+
+void print_file(struct filestr in)
+{
+	for (int i = 0; i < in.depth; i++)
+		printf("    ");
+	printf("| %s\n", in.name);
+}
+
+int main(int argc, char *argv[])
+{
+	char *start = (argc > 1) ? argv[1] : ".";
+	printf("Tree for %s\n",
+			(argc > 1) ? start : "The current directory");
+	process_dir(.name = start, .file_action = print_file,
+			.directory_action = print_dir);
+}
