d7e6b65426a6f07eea13c27e1a511b28f5a87e28
Parham Alvani <parham.alvani@gmial.com>
2015-02-18 09:51:27 +0330

    example-4 is added.
    Former-commit-id: 95651970a4c75d7facf896530677e2b962b52e09

---
+++ GTK/example-4/example-4.c
@@ -0,0 +1,49 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : example-4.c
+ *
+ * [] Creation Date : 18-02-2015
+ *
+ * [] Last Modified : Wed 18 Feb 2015 09:51:12 AM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <gtk/gtk.h>
+
+static void print_hello(GtkWidget *widget, gpointer data)
+{
+	g_print ("Hello World\n");
+}
+
+int main(int argc, char *argv[])
+{
+	GtkBuilder *builder;
+	GObject *window;
+	GObject *button;
+
+	gtk_init(&argc, &argv);
+
+	/* Construct a GtkBuilder instance and load our UI description */
+	builder = gtk_builder_new();
+	gtk_builder_add_from_file(builder, "builder.ui", NULL);
+
+	/* Connect signal handlers to the constructed widgets. */
+	window = gtk_builder_get_object(builder, "window");
+	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+	button = gtk_builder_get_object(builder, "button1");
+	g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+
+	button = gtk_builder_get_object(builder, "button2");
+	g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+
+	button = gtk_builder_get_object(builder, "quit");
+	g_signal_connect(button, "clicked", G_CALLBACK(gtk_main_quit), NULL);
+
+	gtk_main();
+
+	return 0;
+}
+
