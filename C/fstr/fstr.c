328e6fbd4b1d47e3e52c87c2efb15efbe3a31459
Parham Alvani <parham.alvani@gmail.com>
2015-03-06 18:59:58 +0330

    fstr project is added.
    Former-commit-id: 5561291131ce900d90670affc873945017418970

---
+++ C/fstr/fstr.c
@@ -0,0 +1,59 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : fstr.c
+ *
+ * [] Creation Date : 06-03-2015
+ *
+ * [] Last Modified : Fri 06 Mar 2015 06:59:48 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "fstr.h"
+
+struct fstr *fstr_new(const char *str)
+{
+	struct fstr *new;
+
+	new = malloc(sizeof(struct fstr));
+	*new = (struct fstr) {.start = 0, .refs = malloc(sizeof(int))};
+	new->data = malloc(strlen(str) + 1);
+	strcpy(new->data, str);
+	new->data[strlen(str)] = 0;
+	new->end = strlen(str);
+	*new->refs = 1;
+	return new;
+}
+
+struct fstr *fstr_cpy(const struct fstr *in, size_t start, size_t len)
+{
+	struct fstr *new;
+
+	new = malloc(sizeof(struct fstr));
+	*new = *in;
+	new->start += start;
+	if (in->end > new->start + len)
+		new->end = new->start + len;
+	(*new->refs)++;
+	return new;
+}
+
+void fstr_free(struct fstr *in)
+{
+	(*in->refs)--;
+	if (!*in->refs) {
+		free(in->data);
+		free(in->refs);
+	}
+	free(in);
+}
+
+void fstr_show(const struct fstr *in)
+{
+	printf("%.*s\n", (int)(in->end - in->start), &(in->data[in->start]));
+}
---
+++ C/fstr/main.c
@@ -0,0 +1,33 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : main.c
+ *
+ * [] Creation Date : 06-03-2015
+ *
+ * [] Last Modified : Fri 06 Mar 2015 06:55:12 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "fstr.h"
+
+int main(int argc, char *argv[])
+{
+	struct fstr *string = fstr_new("Hello world of Assembly");
+
+	fstr_show(string);
+
+	struct fstr *sub_string = fstr_cpy(string, 0, 10);
+
+	fstr_show(sub_string);
+
+	fstr_free(string);
+
+	fstr_show(sub_string);
+
+	fstr_free(sub_string);
+}
