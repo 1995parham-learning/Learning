752124190daf8c8ad798825dc6bb9d4b8f6113ec
Parham Alvani <parham.alvani@gmial.com>
2015-03-19 01:12:02 +0330

    hello-PI project is added.

---
+++ C/hello-PI/hello-PI.c
@@ -0,0 +1,18 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : hello.c
+ *
+ * [] Creation Date : 19-03-2015
+ *
+ * [] Last Modified : Thu 19 Mar 2015 01:00:20 AM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("hello world of PI\n");
+}
---
+++ C/hello-PI/hello-PI.s
@@ -0,0 +1,41 @@
+	.arch armv6
+	.eabi_attribute 27, 3
+	.eabi_attribute 28, 1
+	.fpu vfp
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 18, 4
+	.file	"hello.c"
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"hello world of PI\000"
+	.text
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 8
+	@ frame_needed = 1, uses_anonymous_args = 0
+	stmfd	sp!, {fp, lr}
+	add	fp, sp, #4
+	sub	sp, sp, #8
+	str	r0, [fp, #-8]
+	str	r1, [fp, #-12]
+	ldr	r0, .L2
+	bl	puts
+	mov	r0, r3
+	sub	sp, fp, #4
+	ldmfd	sp!, {fp, pc}
+.L3:
+	.align	2
+.L2:
+	.word	.LC0
+	.size	main, .-main
+	.ident	"GCC: (Debian 4.6.3-14+rpi1) 4.6.3"
+	.section	.note.GNU-stack,"",%progbits
