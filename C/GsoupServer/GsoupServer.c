d81492b234d7cc048724ce1304d1d9f42b467498
Parham Alvani <parham.alvani@gmial.com>
2015-03-23 01:08:09 +0430

    GsoupServer project improved.
    Former-commit-id: 27eb0f9249c21e23bc9842da6d3714978336da96

--- C/GsoupServer/GsoupServer.c
+++ C/GsoupServer/GsoupServer.c
@@ -5,7 +5,7 @@
  *
  * [] Creation Date : 22-03-2015
  *
- * [] Last Modified : Sun 22 Mar 2015 11:41:02 PM IRDT
+ * [] Last Modified : Mon 23 Mar 2015 01:02:05 AM IRDT
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
@@ -15,29 +15,38 @@
 #include <libsoup/soup.h>
 #include <stdio.h>
 
+static void http_callback(SoupServer *server, SoupMessage *msg,
+		const char *path, GHashTable *query,
+		SoupClientContext *client, gpointer user_data)
+{
+	SoupSession *session;
+	guint status;
+
+	g_print("Sendding request...\n");
+	session = soup_session_new();
+	status = soup_session_send_message(session, msg);
+	g_print("Rquest sent : %d\n", status);
+	
+	soup_message_set_status(msg, status);
+	soup_message_body_complete(msg->response_body);
+}
+
 int main(int argc, char *argv[])
 {
 	SoupServer *server;
 	GError *error = NULL;
-	gboolean status;
 	GMainLoop *loop;
 
 	server = soup_server_new(SOUP_SERVER_SERVER_HEADER,
-			"Parham Server", NULL);
-
-	status = soup_server_listen_all(server, 1373,
+			"Parham-Server-1.0", NULL);
+	soup_server_listen_all(server, 1373,
 			SOUP_SERVER_LISTEN_IPV4_ONLY, &error);
-
-	if (status == FALSE)
-		printf("no ok !\n");
-	else
-		printf("ok !\n");
-
 	if (error) {
 		g_printerr("Unable to create server: %s\n",
 				error->message);
 		exit(1);
 	}
+	soup_server_add_handler(server, NULL, http_callback, NULL, NULL);
 
 	loop = g_main_loop_new(NULL, TRUE);
 	g_main_loop_run(loop);
