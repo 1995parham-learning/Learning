62809e847442df28464e977853b661f3bdd7656f
Parham Alvani <parham.alvani@gmail.com>
2015-03-25 02:37:22 +0430

    Gsource project is added.
    Former-commit-id: fe58b73fb006f5b979c647b8b278aeafeaeafab6

---
+++ C/GSource/main.c
@@ -0,0 +1,20 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : main.c
+ *
+ * [] Creation Date : 24-03-2015
+ *
+ * [] Last Modified : Tue 24 Mar 2015 03:50:30 PM IRDT
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <glib.h>
+
+
+int main(int argc, char *argv[])
+{
+	if (!GLIB_CHECK_VERSION(2, 44, 0))
+		g_print("Glib version is too old\n");
+}
