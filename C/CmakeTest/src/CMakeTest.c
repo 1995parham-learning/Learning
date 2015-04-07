a90c26389c2ce7deb11573780a07bb0db3199acd
Parham Alvani <parham.alvani@gmial.com>
2015-04-08 03:27:15 +0430

    CMakeTest project is added.
    Former-commit-id: 6a9538a6dd944d1d671a47c06da707903a0b2c18

---
+++ C/CmakeTest/src/CMakeTest.c
@@ -0,0 +1,19 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : CMakeTest.c
+ *
+ * [] Creation Date : 08-04-2015
+ *
+ * [] Last Modified : Wed 08 Apr 2015 03:15:28 AM IRDT
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <glib.h>
+
+int main(int argc, char *argv[])
+{
+	g_print("Hello world of cmake and glib\n");
+	return 0;
+}
