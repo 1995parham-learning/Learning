ceadf398c5be34095e7c0b0787a8f955c606d91b
Parham Alvani <parham.alvani@gmial.com>
2015-04-17 10:10:41 +0430

    fibonacci project is added.
    Former-commit-id: 2787b7ade7f08f6c16c7410f542bce43e773c118

---
+++ Python/fibonacci/src/fibonacci.py
@@ -0,0 +1,20 @@
+# In The Name Of God
+# ========================================
+# [] File Name : fibonacci
+#
+# [] Creation Date : 17-04-2015
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+__author__ = 'Parham Alvani'
+
+
+def fibonacci(n):
+    if not isinstance(n, int):
+        return None
+    if n == 0 or n == 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+print(fibonacci(5))
