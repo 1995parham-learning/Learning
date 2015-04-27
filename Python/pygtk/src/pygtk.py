3a37aa2865b695cc772a6db74d90f1d8a03b8f26
Parham Alvani <parham.alvani@gmial.com>
2015-04-28 01:17:17 +0430

    pygtk project improved.

--- Python/pygtk/src/pygtk.py
+++ Python/pygtk/src/pygtk.py
@@ -1,6 +1,6 @@
 # In The Name Of God
 # ========================================
-# [] File Name : pygtk
+# [] File Name : pygtk.py
 #
 # [] Creation Date : 18-04-2015
 #
@@ -14,7 +14,35 @@ from gi.repository import Gtk
 class Base:
     def __init__(self):
         self.window = Gtk.Window()
-        self.window.show()
+        self.window.set_title("Parham Alvani")
+        self.button = Gtk.Button(label="Button-1")
+        self.window.add(self.button)
+        self.button.connect("clicked", Base.hello, None)
+        self.window.show_all()
+        self.window.connect("destroy", Base.exit, None)
+
+    @staticmethod
+    def hello(widget, arg):
+        """
+        :param widget: the widget to operate on
+        :param arg: user-supplied data
+        :type widget: Gtk.Widget
+        :type arg: object
+        :return:
+        """
+        print(widget.get_name())
+        print("hello")
+
+    @staticmethod
+    def exit(widget, arg):
+        """
+        :param widget: the widget to operate on
+        :param arg: user-supplied data
+        :type widget: Gtk.Widget
+        :type arg: object
+        :return:
+        """
+        Gtk.main_quit()
 
     @staticmethod
     def main():
