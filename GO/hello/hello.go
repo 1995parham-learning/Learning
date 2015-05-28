e018ce11810ce97d6828ebb079d89cd325c39bc7
Parham Alvani <parham.alvani@gmail.com>
2015-05-28 10:31:25 +0430

    hello project improved.
    Former-commit-id: 1ceea6d1261064adc1e2f5a4109fecf117891a1c

--- GO/hello/hello.go
+++ GO/hello/hello.go
@@ -1,7 +1,28 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : hello.go
+ *
+ * [] Creation Date : 28-05-2015
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2015 Parham Alvani.
+*/
 package main
 
 import "fmt"
 
+/* with type */
+const M64 int64 = 1 << 20
+/* without type */
+const M = 1 << 20
+
 func main() {
 	fmt.Println("hello world\n")
+	var s, t string = "foo", "bar"
+	fmt.Println(M)
+	fmt.Println(s + t)
 }
