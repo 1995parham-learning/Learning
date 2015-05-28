be5393344ee61367ef2bb6af9e4dac232e21a850
Parham Alvani <parham.alvani@gmail.com>
2015-05-28 11:20:29 +0430

    hello project improved.
    Former-commit-id: 06d88e84bb68427811b22616e3fe5951fd4b2e0d

--- GO/hello/hello.go
+++ GO/hello/hello.go
@@ -22,7 +22,9 @@ const M64 int64 = 1 << 20
 /* without type */
 const M = 1 << 20
 
-/* */
+var ch chan int
+
+/* Point is public but point is private */
 type Point struct {
 	x int
 	y int
@@ -37,11 +39,10 @@ type Stringer interface {
 }
 
 func whileTrue() {
-	i := 0
-	for {
-		i++
+	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 	}
+	ch <- 1
 }
 
 func main() {
@@ -57,6 +58,11 @@ func main() {
 	p2 = Point {2, 3}
 	fmt.Println(p2)
 	
+	/* channel is lock that can store value */
+	ch = make(chan int, 1)
+
 	/* the go statement launches a function call as goroutine */
 	go whileTrue()
+
+	<-ch
 }
