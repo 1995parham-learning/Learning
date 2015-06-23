e9e9abfbeca53885d09c2b3e496312e621ffca02
Parham Alvani <parham.alvani@gmail.com>
2015-06-23 16:11:14 +0430

    web request project is added.
    Former-commit-id: a979e7dec8c98e3252aefb15e13086c3992aad9d

---
+++ NodeJS/request/request.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+
+
+http.get("http://www.varzesh3.com/", function(response) {
+ 	console.log("Got response: " + response.statusCode);
+
+ 	var ws = fs.createWriteStream('response.html');
+ 	response.pipe(ws);
+
+	/* Listen for end of file event of res */
+ 	response.on('end', function(){
+ 		console.log('Finished !');
+ 	});
+
+/* Listen for error event of http get */
+}).on('error', function(e) {
+	console.log("Got error: " + e.message);
+});
