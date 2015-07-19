3fff5ab31277a707ab7f355d0cc301b5ded9628a
Parham Alvani <parham.alvani@gmail.com>
2015-07-20 01:32:01 +0430

    Renamer project improved.

--- Python/renamer/src/renamer.py
+++ Python/renamer/src/renamer.py
@@ -9,22 +9,57 @@
 # =======================================
 __author__ = 'Parham Alvani'
 
-import sys
 import os
 import shutil
+import cmd
 
-if len(sys.argv) > 1:
-    path = sys.argv[1]
-else:
-    path = "."
+try:
+    import termcolor
+except ImportError:
+    termcolor = None
 
-index = 1
-print("{0:=^80}".format("Renaming start"))
-for file in sorted(os.listdir(path)):
-    extension = os.path.splitext(file)[1]
-    if extension.lower() == ".jpg":
-        name = "{0}.jpg".format(index)
-        index += 1
-        shutil.move(file, name)
-        print("mv {0} {1}".format(file, name))
-print("{0:=^80}".format("Renaming end"))
+
+class Rename(cmd.Cmd):
+    def __init__(self):
+        super(Rename, self).__init__()
+        self.path = '.'
+        self.mode = 'pictures'
+        self.intro = "{0:*^80}\n{1:=^80}\n".format("Welcome", "Renamer program for organizing pictures and tv series")
+        self.prompt = "Renamer> [{0}] ".format(self.mode)
+        if termcolor:
+            self.prompt = termcolor.colored(self.prompt, color="red")
+
+    def do_rename(self, line: str):
+        print("{0:=^80}".format("Renaming start"))
+        if self.mode == 'pictures':
+            print("Mode: {0:<80}".format(self.mode))
+            print("Path: {0:<80}".format(self.path))
+            index = 1
+            for file in sorted(os.listdir(self.path)):
+                extension = os.path.splitext(file)[1]
+                if extension.lower() == ".jpg":
+                    name = "{0}.jpg".format(index)
+                    index += 1
+                    shutil.move(file, name)
+                    print("mv {0} {1}".format(file, name))
+        print("{0:=^80}".format("Renaming end"))
+
+    def do_shell(self, line: str):
+        os.system(line)
+
+    def do_quit(self, line: str):
+        return True
+
+    def do_mode(self, line: str):
+        if line.lower() == 'pictures':
+            self.mode = 'pictures'
+        elif line.lower() == 'tv-series':
+            self.mode = 'tv-series'
+        else:
+            print("*** Unknown mode: {0}".format(line))
+
+    do_EOF = do_quit
+
+
+cli = Rename()
+cli.cmdloop()
