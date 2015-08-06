97e6718a5106274642b0172fff5a317ce686afd7
Parham Alvani <parham.alvani@gmail.com>
2015-08-06 23:14:31 +0430

    DjnagoTest project is removed.
    Former-commit-id: 0a5e58193a3eac4bfb4e754f1f6beb4bc05b4a19

--- Python/DjangoTest/Test/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-
-class Product(models.Model):
-    sku = models.CharField(max_length=8, verbose_name='SKU')
-    name = models.CharField(max_length=255)
