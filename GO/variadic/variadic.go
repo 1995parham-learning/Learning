d8f3055d2af80dac27ba7044d6cd4503d8f1ef56
Parham Alvani <parham.alvani@gmail.com>
2015-11-19 11:15:11 +0330

    Go variadic sample was added.
    Former-commit-id: e5db337320f74da97d25413824e2b2e6b7c892d4

---
+++ GO/variadic/variadic.go
@@ -0,0 +1,34 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : variadic.go
+ *
+ * [] Creation Date : 19-11-2015
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+ */
+/*
+ * Copyright (c) 2015 Parham Alvani.
+ */
+package main
+
+import "fmt"
+
+func sums(nums ...int) {
+	fmt.Println("## ##")
+	fmt.Println(nums)
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
+}
+
+func main() {
+	sums(1, 2, 3)
+	sums(2, 3, 4)
+
+	nums := []int{1, 2, 3, 4}
+	sums(nums...)
+}
