568e009ea86aef84ea590a20a0e49715a12bc983
Parham Alvani <parham.alvani@gmail.com>
2016-02-09 17:03:57 +0330

    Fulladdr project structure changed.
    Former-commit-id: 7c9be5dec3adad2c029ad41ee436abbd4fa7854b

--- VHDL/fulladdr/fulladdr.vhd
+++ VHDL/fulladdr/fulladdr.vhd
@@ -12,8 +12,22 @@ entity fulladdr is
 		sum, c_out : out std_logic);
 end entity fulladdr;
 
-architecture arch_fulladdr of fulladdr is
+architecture arch_fulladdr_v1 of fulladdr is
 begin
 	sum <= a xor b xor c_in;
 	c_out <= (a and b) or (a and c_in) or (b and c_in);
-end architecture arch_fulladdr;
+end architecture arch_fulladdr_v1;
+
+architecture arch_fulladdr_v2 of fulladdr is
+	component halfaddr is
+		port(a, b : in std_logic;
+			sum, c_out : out std_logic);
+	end component halfaddr;
+	signal temp1 : std_logic;
+	signal temp2 : std_logic;
+	signal temp3 : std_logic;
+begin
+	h1 : halfaddr port map(a, b, temp1, temp2);
+	h2 : halfaddr port map(temp1, c_in, sum, temp3);
+	c_out <= temp3 or temp2;
+end architecture arch_fulladdr_v2;
