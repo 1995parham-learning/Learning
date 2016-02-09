2355617d7314b12431660725ab20d9bb78057c36
Parham Alvani <parham.alvani@gmail.com>
2016-02-09 17:04:22 +0330

    Fulladdr project structure was changed.

---
+++ VHDL/fulladdr/halfaddr.vhd
@@ -0,0 +1,19 @@
+--------------------------------------------------------------------------------
+-- Author:        Parham Alvani (parham.alvani@gmail.com)
+--
+-- Create Date:   08-02-2016
+-- Module Name:   halfaddr.vhd
+--------------------------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity halfaddr is
+	port(a, b : in std_logic;
+		sum, c_out : out std_logic);
+end entity halfaddr;
+
+architecture arch_halfaddr of halfaddr is
+begin
+	sum <= a xor b;
+	c_out <= a and b;
+end architecture arch_halfaddr;
