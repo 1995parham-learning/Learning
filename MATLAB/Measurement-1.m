37fb622bc0843e039d3a025e29a17eb623049c9b
Parham Alvani <parham.alvani@gmail.com>
2016-02-11 20:12:04 +0330

    Measurement matlab sample issues were corrected.

--- MATLAB/Measurement-1.m
+++ MATLAB/Measurement-1.m
@@ -3,12 +3,12 @@ h = [2.21, 2.13, 2.22];
 R_avg = 0;
 for i = 1:3
 	R(i) = (a(i) * a(i)) / (6 * h(i)) + h(i) / 2;	
-	printf("R(%d): %g\n", i, R(i));
+	printf('R(%d): %g\n', i, R(i));
 	R_avg = R_avg + R(i);
 end
 R_avg = R_avg / 3;
-printf("R_avg: %g\n", R_avg);
+printf('R_avg: %g\n', R_avg);
 for i = 1:3
-	printf("R_abs_%d: %g\n", i, abs(R_avg - R(i)));
-	printf("R_rel_%d: %g\n", i, abs(R_avg - R(i)) / R_avg);
+	printf('R_abs_%d: %g\n', i, abs(R_avg - R(i)));
+	printf('R_rel_%d: %g\n', i, abs(R_avg - R(i)) / R_avg);
 end
