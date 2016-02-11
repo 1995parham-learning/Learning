092a8b485158d91d25954612e7f054ef75b543eb
Parham Alvani <parham.alvani@gmail.com>
2016-02-11 21:00:31 +0330

    Measurement example was improved.

--- MATLAB/Measurement-1.m
+++ MATLAB/Measurement-1.m
@@ -5,10 +5,10 @@ for i = 1:3
 	R(i) = (a(i) * a(i)) / (6 * h(i)) + h(i) / 2;	
 	printf('R(%d): %g\n', i, R(i));
 	R_avg = R_avg + R(i);
-end
+endfor
 R_avg = R_avg / 3;
 printf('R_avg: %g\n', R_avg);
 for i = 1:3
 	printf('R_abs_%d: %g\n', i, abs(R_avg - R(i)));
 	printf('R_rel_%d: %g\n', i, abs(R_avg - R(i)) / R_avg);
-end
+endfor
