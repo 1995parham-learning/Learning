9f548e6ab3a75e099f7cbcb9aa2b8e26a069d98f
Parham Alvani <parham.alvani@gmail.com>
2016-02-11 21:00:09 +0330

    helloworld example was improved.
    Former-commit-id: 37d17ce2b80e7c046e88cab1d7db559ac4571d8a

--- MATLAB/helloworld.m
+++ MATLAB/helloworld.m
@@ -1,15 +1,18 @@
-disp ('hello world');
-var1 = 3.14;
-var1 = 3.14;
+disp('hello world');
 var = 3.14;
 mystring = 'hello';
 a = 10;
 b = 13 / 3 ;
-row = [ 1 2 3.2 4 ];
-col = [ 1; 2; -6; 4];
-size(row);
-size(col);
-length(row);
-length(col);
-mat = [ 1 2 ; 3 4 ];
-str = ['hello, i am ','elahe'];
+row = [1, 2, 3.2, 4];
+col = [1; 2; -6; 4];
+
+# lines without ; at their end printed
+# by octave :)
+size(row)
+size(col)
+length(row)
+length(col)
+
+mat = [1, 2; 3, 4];
+str = ['Hello, I am ', 'Parham Alvani'];
+printf("%s\n", str);
