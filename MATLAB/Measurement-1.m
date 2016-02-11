bc92c9c26a249d65bb20fb925674a991bfb833c9
Parham Alvani <parham.alvani@gmail.com>
2016-02-11 21:07:01 +0330

    Measurement example was improved.
    Former-commit-id: 1477e0ce2e8f95f25688f46366e869f71f4bd232

--- MATLAB/Measurement-1.m
+++ MATLAB/Measurement-1.m
@@ -1,14 +1,14 @@
 a = [37.24, 37.00, 36.40];
 h = [2.21, 2.13, 2.22];
 R_avg = 0;
-for i = 1:3
+for i = 1:length(a)
 	R(i) = (a(i) * a(i)) / (6 * h(i)) + h(i) / 2;	
 	printf('R(%d): %g\n', i, R(i));
 	R_avg = R_avg + R(i);
 endfor
-R_avg = R_avg / 3;
+R_avg = R_avg / length(a);
 printf('R_avg: %g\n', R_avg);
-for i = 1:3
+for i = 1:length(a)
 	printf('R_abs_%d: %g\n', i, abs(R_avg - R(i)));
 	printf('R_rel_%d: %g\n', i, abs(R_avg - R(i)) / R_avg);
 endfor
