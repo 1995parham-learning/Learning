f0e7b9e7c9d3a3e8e5168dbf01e85e4b4a4da7f9
Parham Alvani <parham.alvani@gmail.com>
2016-02-12 21:22:21 +0330

    Signal definition was corrected.

--- VHDL/sr-latch/sr-latch.vhd
+++ VHDL/sr-latch/sr-latch.vhd
@@ -9,14 +9,14 @@ use IEEE.std_logic_1164.all;
 
 entity sr_latch is
 	port(s, r : in std_logic;
-		q, q_ : out std_logic);
+		q, q_not : out std_logic);
 end entity sr_latch;
 
 architecture arch_sr_latch of sr_latch is
-	fq, fq_ : signal std_logic;
+	signal fq, fq_not : std_logic;
 begin
-	fq <= r nor fq_;
+	fq <= r nor fq_not;
 	q <= fq;
-	fq_ <= s nor fq;
-	q_ <= fq_;
+	fq_not <= s nor fq;
+	q_not <= fq_not;
 end architecture arch_sr_latch;
