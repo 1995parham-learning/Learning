5cea139b3c0f65dfcdaaf4456aade07af7708db0
Parham Alvani <parham.alvani@gmail.com>
2016-02-14 23:26:09 +0330

    Architecture was added into testbench.

--- VHDL/sr-latch/sr-latch_t.vhd
+++ VHDL/sr-latch/sr-latch_t.vhd
@@ -13,7 +13,7 @@ end entity;
 architecture arch_sr_latch_t of sr_latch_t is
 	signal s, r, q, q_not : std_logic;
 begin
-	sr_latch_1 : entity work.sr_latch port map(s, r, q, q_not);
+	sr_latch_1 : entity work.sr_latch(arch_sr_latch) port map(s, r, q, q_not);
 	s <= '1', '0' after 5 ns, '0' after 10 ns, '1' after 15 ns;
 	r <= '0', '1' after 5 ns, '0' after 10 ns, '1' after 15 ns;
 end architecture arch_sr_latch_t;
