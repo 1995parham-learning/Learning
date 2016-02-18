16c7ccc380536b7b4f4e5c96be55fac096db976d
Parham Alvani <parham.alvani@gmail.com>
2016-02-18 20:13:41 +0330

    GP-Lab-1 erroring script was improved.

--- octave/Measurement-2.m
+++ octave/Measurement-2.m
@@ -8,9 +8,12 @@ d_avg = 0;
 for i = 1:length(d)
 	d_avg = d_avg + d(i);
 endfor
-d_avg = d_avg / length(a);
-printf('d_avg: %g\n', td_avg);
-for i = 1:length(a)
+d_avg = d_avg / length(d);
+printf('d_avg: %g\n', d_avg);
+d_x_m = 0;
+for i = 1:length(d)
 	printf('d_abs_%d: %g\n', i, abs(d_avg - d(i)));
 	printf('d_rel_%d: %g\n', i, abs(d_avg - d(i)) / d_avg);
+	d_x_m = d_x_m + abs(d_avg - d(i));
 endfor
+printf("d_x_m: %g\n", d_x_m / length(d));
