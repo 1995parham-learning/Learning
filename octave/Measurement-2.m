aa76a12fcfd77077c55c45a5a4cd253389d98684
Parham Alvani <parham.alvani@gmail.com>
2016-02-18 19:47:39 +0330

    GP-Lab-1 erroring script was improved.
    Former-commit-id: e54af2b35bff13f8971e92784ed70db6b0d79ab9

--- octave/Measurement-2.m
+++ octave/Measurement-2.m
@@ -1,10 +1,15 @@
-d = [37.24, 37.00, 36.40];
+printf("Welcome to 3 number GP-1-Lab erroring script :D\n");
+len = input("How many number do you have? ");
+d = [];
+for i = 1:len
+	d(i) = input(": ");
+endfor
 d_avg = 0;
 for i = 1:length(d)
 	d_avg = d_avg + d(i);
 endfor
-R_avg = R_avg / length(a);
-printf('d_avg: %g\n', R_avg);
+d_avg = d_avg / length(a);
+printf('d_avg: %g\n', td_avg);
 for i = 1:length(a)
 	printf('d_abs_%d: %g\n', i, abs(d_avg - d(i)));
 	printf('d_rel_%d: %g\n', i, abs(d_avg - d(i)) / d_avg);
