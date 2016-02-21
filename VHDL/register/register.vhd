5cf4daf0a6bdefb1d8364e8698f3b23d49c8fbda
Parham Alvani <parham.alvani@gmail.com>
2016-02-22 01:20:48 +0330

    N-bit register VHDL sample code was added.
    Former-commit-id: 5b5e0d372fe5d9109699d99d3af52cd4eecf90dc

---
+++ VHDL/register/register.vhd
@@ -0,0 +1,31 @@
+--------------------------------------------------------------------------------
+-- Author:        Parham Alvani (parham.alvani@gmail.com)
+--
+-- Create Date:   22-02-2016
+-- Module Name:   register.vhd
+--------------------------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity n_register is
+	generic (N : integer := 8);
+	port (d : in std_logic_vector(N - 1 downto 0);
+		clk, s, r : in std_logic;
+		q : out std_logic_vector(N - 1 downto 0));
+end entity n_register;
+
+architecture beh_arch_n_register of n_register is
+begin
+	process (clk)
+	begin
+		if clk = '1' and clk`event then
+			if s = '1' then
+				q <= (others => '1');
+			elsif r = '1' then
+				q <= (others => '0');
+			else
+				q <= d;
+			end if;
+		end if;
+	end process;
+end architecture beh_arch_n_register;
