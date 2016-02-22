19d462528cb079a1ad3be5e4b8fba79eae47b9db
Parham Alvani <parham.alvani@gmail.com>
2016-02-22 14:42:44 +0330

    D Latch new architecture was added and issues were corrected.

--- VHDL/d-latch/d-latch.vhd
+++ VHDL/d-latch/d-latch.vhd
@@ -8,18 +8,29 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity d_latch is
-	port (d : in std_logic;
+	port (d, clk : in std_logic;
 		q, q_not : out std_logic);
 end entity d_latch;
 
-architecture arch_d_latch of d_latch is
+architecture str_arch_d_latch of d_latch is
 	component sr_latch is
-		port (s, r : in std_logic;
+		port (s, r, clk : in std_logic;
 			q, q_not : buffer std_logic);
 	end component sr_latch;
 	signal d_not : std_logic;
 	for all:sr_latch use entity work.sr_latch(arch_sr_latch);
 begin
 	d_not <= not d;
-	sr_latch_1 : sr_latch port map (d, d_not, q, q_not);
-end architecture arch_d_latch;
+	sr_latch_1 : sr_latch port map (d, d_not, clk, q, q_not);
+end architecture str_arch_d_latch;
+
+architecture beh_arch_d_latch of d_latch is
+begin
+	process (clk, d)
+	begin
+		if clk = '1' then
+			q <= d;
+			q_not <= not d;
+		end if;
+	end process;
+end architecture beh_arch_d_latch;
