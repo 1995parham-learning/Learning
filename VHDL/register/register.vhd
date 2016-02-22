eace64486dac796c537cb05fe581b781f2aa086b
Parham Alvani <parham.alvani@gmail.com>
2016-02-22 15:27:23 +0330

    Synchronous and Asynchronous reset and set was added.
    Former-commit-id: 79ef63eb2c7a289f816145880238f124a7e28120

--- VHDL/register/register.vhd
+++ VHDL/register/register.vhd
@@ -10,18 +10,24 @@ use IEEE.std_logic_1164.all;
 entity n_register is
 	generic (N : integer := 8);
 	port (d : in std_logic_vector(N - 1 downto 0);
-		clk, s, r : in std_logic;
+		clk, s_sync, r_sync : in std_logic;
+		s_async, r_async : in std_logic;
 		q : out std_logic_vector(N - 1 downto 0));
 end entity n_register;
 
 architecture beh_arch_n_register of n_register is
 begin
-	process (clk)
+	process (clk, s_async, r_async)
 	begin
+		if s_async = '1' then
+			q <= (others => '1');
+		elsif r_async = '1' then
+			q <= (others => '0');
+		end if;
 		if clk = '1' and clk'event then
-			if s = '1' then
+			if s_sync = '1' then
 				q <= (others => '1');
-			elsif r = '1' then
+			elsif r_sync = '1' then
 				q <= (others => '0');
 			else
 				q <= d;
--- VHDL/register/register_t.vhd
+++ VHDL/register/register_t.vhd
@@ -14,13 +14,14 @@ architecture arch_n_register_t of n_register_t is
 	component n_register is
 		generic (N : integer := 8);
 		port (d : in std_logic_vector(N - 1 downto 0);
-			clk, s, r : in std_logic;
+			clk, s_sync, r_sync : in std_logic;
+			s_async, r_async : in std_logic;
 			q : out std_logic_vector(N - 1 downto 0));
 	end component n_register;
 	for all:n_register use entity work.n_register(beh_arch_n_register);
-	signal clk, s, r : std_logic;
+	signal clk, s_sync, r_sync, s_async, r_async : std_logic;
 	signal d, q : std_logic_vector(0 downto 0);
 begin
-	reg_1 : n_register generic map (1) port map (d, clk, s, r, q);
+	reg_1 : n_register generic map (1) port map (d, clk, s_sync, r_sync, s_async, r_async, q);
 	d <= "0", "1" after 100 ns, "0" after 200 ns;
 end architecture arch_n_register_t;
