7e1dbfd5234216c0edc307d196af8f3f8e9bd453
Parham Alvani <parham.alvani@gmail.com>
2016-02-24 04:18:01 +0330

    Finally ... I am rocked :)))

--- VHDL/crc/crc_t.vhd
+++ VHDL/crc/crc_t.vhd
@@ -11,20 +11,25 @@ entity crc_t is
 end entity;
 
 architecture arch_crc_t of crc_t is
+	-- G(x) Function
+	constant G : std_logic_vector(3 downto 0) := "1011";
+	-- CRC Component definition
 	component crc is
 		generic (g : std_logic_vector);
 		port (d, clk : in std_logic;
 			r : buffer std_logic_vector(g'high - 1 downto g'low));
 	end component;
+	-- Middle signals
 	signal clk, d : std_logic := '0';
 	signal r : std_logic_vector(2 downto 0);
 	signal data : std_logic_vector(10 downto 0) := "10101100000";
 	signal run : std_logic := '1';
+	-- CRC Component configuration
 	for all:crc use entity work.crc(arch_crc);
 begin
 	clk <= not clk after 50 ns when run = '1';
 	
-	crc_1 : crc generic map ("1011") port map (d, clk, r);
+	crc_1 : crc generic map (G) port map (d, clk, r);
 	
 	process (clk)
 		variable I : natural := data'high + 1;
