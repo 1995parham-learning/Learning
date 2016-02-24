8ca4718779b8d7620d326c9b0958cb18d49684c4
Parham Alvani <parham.alvani@gmail.com>
2016-02-24 04:01:43 +0330

    CRC testbench was changed again ...
    Former-commit-id: fffaf907a39f85cadb94e9439e44e9a6ff887357

--- VHDL/crc/crc_t.vhd
+++ VHDL/crc/crc_t.vhd
@@ -16,24 +16,23 @@ architecture arch_crc_t of crc_t is
 		port (d, clk : in std_logic;
 			r : buffer std_logic_vector(g'high - 1 downto g'low));
 	end component;
-	signal clk : std_logic := '0';
-	signal d : std_logic := '1';
-	signal r : std_logic_vector(4 downto 0);
-	signal data : std_logic_vector(9 downto 0) := "1010001101";
+	signal clk, d : std_logic := '0';
+	signal r : std_logic_vector(2 downto 0);
+	signal data : std_logic_vector(10 downto 0) := "10101100000";
 	signal run : std_logic := '1';
 	for all:crc use entity work.crc(arch_crc);
 begin
 	clk <= not clk after 50 ns when run = '1';
 	
-	crc_1 : crc generic map ("110101") port map (d, clk, r);
+	crc_1 : crc generic map ("1011") port map (d, clk, r);
 	
 	process (clk)
-		variable I : natural := data'low + 1;
+		variable I : natural := data'high + 1;
 	begin
 		if clk = '1' and clk'event then
-			if I <= data'high then
+			if I > data'low then
+				I := I - 1;
 				d <= data(I);
-				I := I + 1;
 			else
 				run <= '0';
 			end if;
