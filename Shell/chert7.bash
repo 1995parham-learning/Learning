f3de5e216a31b0b078ecf92ba444d7d3a942b4da
Parham Alvani <parham.alvani@gmail.com>
2016-03-08 02:46:50 +0330

    Chert sample of bash was added.
    Former-commit-id: a187a066e981820388ba75691184a1d682d239c5

---
+++ Shell/chert7.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : chert7.bash
+#
+# [] Creation Date : 08-03-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+mkdir $HOME/directory1
+echo "Hello World" >> $HOME/directory1/8
+mkdir $HOME/directory2
+cp $HOME/directory1/8 $HOME/directory2/
+rm $HOME/directory1/8
