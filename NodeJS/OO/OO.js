aaee04f60b8c14a792c4f6558a2a3bbd689ef9a1
Parham Alvani <parham.alvani@gmail.com>
2016-03-09 06:25:53 +0330

    NodeJS Object Oriented sample was improved.

--- NodeJS/OO/OO.js
+++ NodeJS/OO/OO.js
@@ -11,6 +11,7 @@
 /*
  * Copyright (c) 2015 Parham Alvani.
 */
+/* Constructor Pattern */
 function Person(name, age, work) {
 	this.name = name;
 	this.age = age;
@@ -21,3 +22,20 @@ function Person(name, age, work) {
 }
 p1 = new Person("Parham Alvani", 21, "Student");
 console.log(p1.sayName());
+
+/* Constructor/Prototype Pattern */
+function NewPerson(name, age, work) {
+	this.name = name;
+	this.age = age;
+	this.work = work;
+}
+
+NewPerson.prototype = {
+	constructor: Person,
+	sayName : function() {
+		return "My name is " + this.name;
+	}
+}
+
+p2 = new NewPerson("Parham Alvani :)", 22, "Collegian");
+console.log(p2.sayName());
