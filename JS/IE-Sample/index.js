6976cd491eca407b290fb9e99ab98b157e9fd75a
Parham Alvani <parham.alvani@gmail.com>
2016-05-02 00:48:21 +0430

    Sample project was completed.
    Former-commit-id: 6e4115a3d70e451a11652ac3d8d775169893e4f8

--- JS/IE-Sample/index.js
+++ JS/IE-Sample/index.js
@@ -12,13 +12,18 @@ function createMatrixInput() {
     var m = parseInt(document.getElementById("inputM").value);
     var k = parseInt(document.getElementById("inputK").value);
 
+    var i, j, tr, td, tdInput, label;
+
+    label = document.createElement("label");
+    label.innerHTML = "A:";
+    document.getElementById("matrix").appendChild(label);
     var table1 = document.createElement("table");
-    for (var i = 0; i < n; i++) {
-        var tr = document.createElement("tr");
-        for (var j = 0; j < m; j++) {
-            var td = document.createElement("td");
-            var tdInput = document.createElement("input");
-            tdInput.id = "table2Input" + i + j;
+    for (i = 0; i < n; i++) {
+        tr = document.createElement("tr");
+        for (j = 0; j < m; j++) {
+            td = document.createElement("td");
+            tdInput = document.createElement("input");
+            tdInput.id = "table1Input" + i + j;
             td.appendChild(tdInput);
             tr.appendChild(td);
         }
@@ -26,12 +31,15 @@ function createMatrixInput() {
     }
     document.getElementById("matrix").appendChild(table1);
 
+    label = document.createElement("label");
+    label.innerHTML = "B:";
+    document.getElementById("matrix").appendChild(label);
     var table2 = document.createElement("table");
-    for (var i = 0; i < m; i++) {
-        var tr = document.createElement("tr");
-        for (var j = 0; j < k; j++) {
-            var td = document.createElement("td");
-            var tdInput = document.createElement("input");
+    for (i = 0; i < m; i++) {
+        tr = document.createElement("tr");
+        for (j = 0; j < k; j++) {
+            td = document.createElement("td");
+            tdInput = document.createElement("input");
             tdInput.id = "table2Input" + i + j;
             td.appendChild(tdInput);
             tr.appendChild(td);
@@ -39,4 +47,34 @@ function createMatrixInput() {
         table2.appendChild(tr);
     }
     document.getElementById("matrix").appendChild(table2);
+
+
+    label = document.createElement("label");
+    label.innerHTML = "C:";
+    document.getElementById("matrix").appendChild(label);
+    var table3 = document.createElement("table");
+    for (i = 0; i < n; i++) {
+        tr = document.createElement("tr");
+        for (j = 0; j < k; j++) {
+            td = document.createElement("td");
+            tdInput = document.createElement("input");
+            tdInput.id = "table3Input" + i + j;
+            tdInput.onclick = function (r, c, m) {
+                return function () {
+                    var result = 0;
+                    var i;
+                    for (i = 0; i < m; i++) {
+                        var v1 = parseInt(document.getElementById("table1Input" + r + i).value);
+                        var v2 = parseInt(document.getElementById("table2Input" + i + c).value);
+                        result = result + v1 * v2;
+                    }
+                    document.getElementById("table3Input" + r + c).value = result;
+                };
+            }(i, j, m);
+            td.appendChild(tdInput);
+            tr.appendChild(td);
+        }
+        table3.appendChild(tr);
+    }
+    document.getElementById("matrix").appendChild(table3);
 }
