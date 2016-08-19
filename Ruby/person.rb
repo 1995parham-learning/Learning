c987c079dc5f0445d64ff0f346b8aea41f89c7b6
Parham Alvani <parham.alvani@gmail.com>
2016-08-20 02:51:53 +0430

    Improve our ruby samples formatting and styles.

--- Ruby/person.rb
+++ Ruby/person.rb
@@ -1,13 +1,16 @@
 class Person
-	attr_accessor :name, :age, :gender
-	def hello(i)
-		i.times do
-			puts "hello"
-		end
-	end
+  # This class provides person in our project.
+  # Person is a abstraction for people in our
+  # life who ...
+  attr_accessor :name, :age, :gender
+  def hello(i)
+    i.times do
+      puts 'hello'
+    end
+  end
 end
 person_instance = Person.new
-person_instance.name = "Parham"
+person_instance.name = 'Parham'
 puts person_instance
 person_instance.hello(3)
 
