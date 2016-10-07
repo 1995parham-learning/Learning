a36a1a6963436e296109e23df24f10cd89637e99
Parham Alvani <parham.alvani@gmail.com>
2016-10-07 09:28:26 +0330

    Discovery sample code was improved.
    Former-commit-id: cbebb0726412f720cd9d6393f91a3476c5e4bb08

--- Python/bluetooth/sample.py
+++ Python/bluetooth/sample.py
@@ -6,13 +6,10 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
-
-
-# simple inquiry example
 import bluetooth
 
 nearby_devices = bluetooth.discover_devices(lookup_names=True)
 print("found %d devices" % len(nearby_devices))
 
 for addr, name in nearby_devices:
-        print("  %s - %s" % (addr, name))
+        print("*  %s - %s" % (addr, name))
