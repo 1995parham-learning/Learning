7349725c7c2aadbbcf58dd16a264983c54287b24
Parham Alvani <parham.alvani@gmail.com>
2016-12-13 12:42:22 +0330

    Many chert sample was added.
    Former-commit-id: efc47f2c26fbd95bf259280883c5e5365c580473

---
+++ Shell/chert-2-6.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# In The Name Of God
+# ========================================
+# [] File Name : chert-2-6.sh
+#
+# [] Creation Date : 07-12-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+ls -t /usr/sbin | head -8
