de6d9aa0edfc547a01a6248ffa5e6c45bc036402
Parham Alvani <parham.alvani@gmail.com>
2016-12-13 12:30:54 +0330

    Chert samples were improved.
    Former-commit-id: d005b21b2f4587a0107b52427c3f3e143f2e7675

--- Shell/chert-2-7.sh
+++ Shell/chert-2-7.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 # In The Name Of God
 # ========================================
-# [] File Name : chert4.bash
+# [] File Name : chert-2-7.sh
 #
 # [] Creation Date : 08-03-2016
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
-counter=0
-for dir in `ls /bin --sort=time --time=atime`; do
-	let counter=$counter+1
-	echo "$counter: $dir"
-	if [ $counter -eq 10 ]; then
-		exit
-	fi
-done
+mkdir /home/directory1
+echo "Hello World" >> /home/directory1/8
+mkdir /home/directory2
+cp /home/directory1/8 /home/directory2/
+rm /home/directory1/8
