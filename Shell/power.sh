f0ae4e8cd70b717165bf8c317aa04d935e2b493c
Parham Alvani <parham.alvani@gmail.com>
2017-01-06 14:26:06 +0330

    Power script was improved in order to have functions.
    Former-commit-id: a460cc95601795641a6ae0511a2c91d93e6a7260

--- Shell/power.sh
+++ Shell/power.sh
@@ -7,13 +7,20 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
-a=$1
-b=$2
 
-answer='1'
+power() {
+	a=$1
+	b=$2
+	answer='1'
 
-for i in `seq 1 $b`; do
-	answer=$(($a * $answer))
-done
+	for i in `seq 1 $b`; do
+		answer=$(($a * $answer))
+	done
 
-echo $answer
+	echo $answer
+}
+
+# Main script (!) Let's test our power function
+echo `power 2 4`
+echo `power 4 2`
+echo `power 3 4`
