223ab9089074da995f1e4cd2bdf40d70b16ff90c
Parham Alvani <parham.alvani@gmail.com>
2017-01-13 20:07:10 +0330

    Threads sample was added.
    Former-commit-id: 59c2430a3f6a5819d88a5ccda945c0c20a0f2960

---
+++ C/Threads/Threads.c
@@ -0,0 +1,27 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : Threads.c
+ *
+ * [] Creation Date : 13-01-2017
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2017 Parham Alvani.
+*/
+#include <threads.h>
+#include <stdio.h>
+
+int run(void *arg)
+{
+	printf("Hello world of C11 threads.");
+}
+
+int main(int argc, const char *argv[])
+{
+	thrd_t thread;
+
+	thrd_create(&thread, run, NULL);
+}
