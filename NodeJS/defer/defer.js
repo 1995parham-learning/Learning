f6c8445b9aafeec30e9e64b059b77fd82e8006c3
Parham Alvani <parham.alvani@gmail.com>
2017-05-02 08:34:14 +0430

    Defer example was added.

---
+++ NodeJS/defer/defer.js
@@ -0,0 +1,15 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 02-05-2017
+ * |
+ * | File Name:     defer.js
+ * +===============================================
+ */
+
+process.nextTick(() => console.log('tick tack'));
+
+for (let i = 0; i < 10; i++) {
+  console.log('Hello and How are you bro');
+}
