e675fbd958d6f6d5b21f4fc7922cfde8c330c6d0
Parham Alvani <parham.alvani@gmail.com>
2017-05-13 08:10:44 +0430

    IssueList class was added.
    Former-commit-id: e66a465a1e0c852ab39673803c21761e14757097

---
+++ NodeJS/React/static/App.jsx
@@ -0,0 +1,11 @@
+const contentNode = document.getElementById('contents');
+
+class IssueList extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the issue list.</div>
+    );
+  }
+}
+
+ReactDOM.render(<IssueList />, contentNode);
