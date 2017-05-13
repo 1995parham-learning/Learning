d001395b15898c0d04350639bbf7e76c407c51b9
Parham Alvani <parham.alvani@gmail.com>
2017-05-13 22:53:35 +0430

    React sample was improved.

--- NodeJS/React/static/App.jsx
+++ NodeJS/React/static/App.jsx
@@ -1,5 +1,13 @@
 const contentNode = document.getElementById('contents');
 
+const issues = [
+  {
+  id: 1, status: 'Open', owner: 'Raven',
+  created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+  title: 'Error in console when clicking Add'
+  }
+]
+
 class IssueFilter extends React.Component {
   render() {
     return (
@@ -10,9 +18,42 @@ class IssueFilter extends React.Component {
 
 class IssueTable extends React.Component {
   render() {
+    const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
     return (
-      <div>This is a placeholder for a table of Issues.</div>
+      <table className="bordered-table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Status</th>
+            <th>Owner</th>
+            <th>Created</th>
+            <th>Effort</th>
+            <th>Completion Date</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {issueRows}
+        </tbody>
+      </table>
     );
+  }
+}
+
+class IssueRow extends React.Component {
+  render() {
+    const issue = this.props.issue;
+    return (
+      <tr>
+        <td>{issue.id}</td>
+        <td>{issue.status}</td>
+        <td>{issue.owner}</td>
+        <td>{issue.created.toDateString()}</td>
+        <td>{issue.effort}</td>
+        <td>{issue.completionDate ? issue.completionDate.toDateString() : ''}</td>
+        <td>{issue.title}</td>
+      </tr>
+    )
   }
 }
 
@@ -31,7 +72,7 @@ class IssueList extends React.Component {
         <h1>Issue Tracker</h1>
         <IssueFilter />
         <hr />
-        <IssueTable />
+        <IssueTable issues={issues} />
         <hr />
         <IssueAdd />
       </div>
