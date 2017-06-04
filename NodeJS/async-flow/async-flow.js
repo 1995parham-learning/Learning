0fd95d7879d8411f7e32a14131a724930432cd5f
Parham Alvani <parham.alvani@gmail.com>
2017-06-04 13:10:24 +0430

    Asynchronous flow example of node was added.

---
+++ NodeJS/async-flow/async-flow.js
@@ -0,0 +1,22 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 04-06-2017
+ * |
+ * | File Name:     async-flow.js
+ * +===============================================
+ */
+const co = require('co');
+
+
+co(function* () {
+  var res = yield [
+    Promise.resolve(1),
+    Promise.resolve(2),
+    Promise.resolve(3),
+  ];
+  console.log(res);
+}).then(() => {
+  console.log('async flow was completed.');
+});
