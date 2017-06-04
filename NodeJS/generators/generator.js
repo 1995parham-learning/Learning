648fff62b2b7d6b1d15b09e1db3939e9f6103c96
Parham Alvani <parham.alvani@gmail.com>
2017-06-04 11:39:21 +0430

    Fibonacci generator was added.

--- NodeJS/generators/generator.js
+++ NodeJS/generators/generator.js
@@ -19,3 +19,26 @@ const newFruitGenerator = fruitGenerator();
 console.log(newFruitGenerator.next());
 console.log(newFruitGenerator.next());
 console.log(newFruitGenerator.next());
+
+function* fibonacciGenerator() {
+  let a = 1;
+  let b = 1;
+  let n = 1;
+
+  while (true) {
+    if (n % 2) {
+      yield a;
+      a = a + b;
+      n++;
+    } else {
+      yield b;
+      b = a + b;
+      n++;
+    }
+  }
+}
+
+const newFibonacciGenerator = fibonacciGenerator();
+for (let i = 0; i < 50; i++) {
+  console.log(newFibonacciGenerator.next());
+}
