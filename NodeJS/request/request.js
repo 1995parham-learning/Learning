6c83ca57865d664f4e2b6b373e2f5ef821841195
Parham Alvani <parham.alvani@gmail.com>
2017-06-11 02:51:05 +0430

    Improves request example.

--- NodeJS/request/request.js
+++ NodeJS/request/request.js
@@ -2,18 +2,19 @@ var http = require('http');
 var fs = require('fs');
 
 
+/* Get request to varzesh3 based on pipes */
 http.get("http://www.varzesh3.com/", function(response) {
- 	console.log("Got response: " + response.statusCode);
+  console.log("Got response: " + response.statusCode);
 
- 	var ws = fs.createWriteStream('response.html');
- 	response.pipe(ws);
+  var ws = fs.createWriteStream('response.html');
+  response.pipe(ws);
 
-	/* Listen for end of file event of res */
- 	response.on('end', function(){
- 		console.log('Finished !');
- 	});
+  /* Listen for end of file event of res */
+  response.on('end', function(){
+    console.log('Finished !');
+  });
 
-/* Listen for error event of http get */
+  /* Listen for error event of http get */
 }).on('error', function(e) {
-	console.log("Got error: " + e.message);
+  console.log("Got error: " + e.message);
 });
