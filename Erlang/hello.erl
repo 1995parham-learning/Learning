5e66a0f76af6219e2c11e37869e2166fdc52ad40
Parham Alvani <parham.alvani@gmail.com>
2017-09-14 21:57:34 +0430

    Adds hello module.

---
+++ Erlang/hello.erl
@@ -0,0 +1,19 @@
+%%%
+%%% In The Name Of God
+%%% ========================================
+%%% [] File Name : hello.erl
+%%%
+%%% [] Creation Date : 14-09-2017
+%%%
+%%% [] Created By : Parham Alvani <parham.alvani@gmail.com>
+%%% =======================================
+%%%
+%%% #######################################
+%%%
+%%% Copyright (c)  2017 Parham Alvani.
+%%%
+-module(hello).
+-export([pie/0]).
+
+pie() ->
+	io:format("Hello").
