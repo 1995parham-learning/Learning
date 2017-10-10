fc22e308cc46f770e7b05d3a9306d07bace20343
Parham Alvani <parham.alvani@gmail.com>
2017-10-10 05:29:10 +0330

    Adds test for channel based implementation of mutex.

---
+++ Go/muxtex/ch_mutex_test.go
@@ -0,0 +1,34 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 10-10-2017
+ * |
+ * | File Name:     ch_mutex_test.go
+ * +===============================================
+ */
+package mutex
+
+import "testing"
+
+func TestOne(t *testing.T) {
+	m := NewChannelMutex()
+	f := 0
+	t.Log(t.Name)
+	go func() {
+		m.Acquire()
+		t.Log("Thread-1")
+		f = 1
+		m.Release()
+	}()
+	for {
+		m.Acquire()
+		t.Log("Thread-2")
+		if f == 1 {
+			m.Release()
+			break
+		} else {
+			m.Release()
+		}
+	}
+}
