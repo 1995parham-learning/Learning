9b52bfff524446fe836e5701804eefa02eb58265
Parham Alvani <parham.alvani@gmail.com>
2017-10-15 21:35:11 +0330

    Init function example of Go was added.
    Former-commit-id: fd64b283087cb8ef026838c274682b380ffe969b

---
+++ Go/init/main.go
@@ -0,0 +1,21 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 15-10-2017
+ * |
+ * | File Name:     main.go
+ * +===============================================
+ */
+
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Printf("Init example of go is starting\n")
+}
+
+func main() {
+	fmt.Printf("Main\n")
+}
