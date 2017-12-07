814d2258810e01d9b0d26d32dbb9fcfbca9d548a
Parham Alvani <parham.alvani@gmail.com>
2017-12-07 09:33:33 +0330

    Improves roy guest :).

--- Go/roy/roy.go
+++ Go/roy/roy.go
@@ -10,18 +10,31 @@
 
 package main
 
-import "fmt"
-
-const N int = 4
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	var acc float64
-	for i := 1; i <= N; i++ {
-		term := 1.0 / float64(i)
-		acc += term * term
-		for j := 1; j < i; j++ {
-			acc *= -1
-		}
-		fmt.Printf("%d: %g\n", i, acc)
+	var n = 4
+
+	if len(os.Args) > 1 {
+		n, _ = strconv.Atoi(os.Args[1])
 	}
+
+	var acc = 1.0
+	fmt.Printf("1")
+
+	for i := 2; i <= n; i++ {
+		term := 1.0 / float64(i*i)
+
+		for j := 1; j < i; j++ {
+			term *= -1
+		}
+		acc += term
+
+		fmt.Printf(" + (-1)^%d * (1/%d)", i-1, i*i)
+	}
+	fmt.Printf("%d: %g\n", n, acc)
 }
