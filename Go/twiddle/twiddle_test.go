4406614ce7fcbff745faf027a571e7907a60aed1
Parham Alvani <parham.alvani@gmail.com>
2017-12-12 23:41:56 +0330

    Adds twiddle algorithm implementation.
    Former-commit-id: 8ca9c0a38d6606336dfd53c9b8c6e208396cdffd

---
+++ Go/twiddle/twiddle_test.go
@@ -0,0 +1,47 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 12-12-2017
+ * |
+ * | File Name:     twiddle_test.go
+ * +===============================================
+ */
+
+package twiddle
+
+import (
+	"testing"
+)
+
+func TestBasic1(t *testing.T) {
+	tw := New(2, 2)
+
+	b := tw.Next()
+	for _, v := range b {
+		if !v {
+			t.Fatalf("excpeting true found %v", v)
+		}
+	}
+}
+
+func TestBasic2(t *testing.T) {
+	tw := New(1, 2)
+
+	var table = []struct {
+		ex [2]bool
+	}{
+		{[2]bool{false, true}},
+		{[2]bool{true, false}},
+	}
+
+	i := 0
+	for b := tw.Next(); b != nil; b = tw.Next() {
+		for j := 0; j < len(b); j++ {
+			if table[i].ex[j] != b[j] {
+				t.Fatalf("excepting %v found %v", table[i].ex[j], b[j])
+			}
+		}
+		i++
+	}
+}
