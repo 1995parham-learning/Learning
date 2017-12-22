e4e2d7e12d585df8eb7646feec5f37d62edccf5c
Parham Alvani <parham.alvani@gmail.com>
2017-12-22 10:28:43 +0330

    Let's do it in multiple file.

---
+++ Go/go-c/hello.h
@@ -0,0 +1,17 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : hello.h
+ *
+ * [] Creation Date : 22-12-2017
+ *
+ * [] Created By : Parham Alvani <parham.alvani@gmail.com>
+ * =======================================
+*/
+/*
+* Copyright (c)  2017 Parham Alvani.
+*/
+#ifndef HELLO_H
+#define HELLO_H
+void say_hello(const char *name);
+#endif
