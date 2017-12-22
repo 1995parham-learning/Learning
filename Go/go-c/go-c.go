e37a8cc88d0e317404b933f3c41ce75de819a60e
Parham Alvani <parham.alvani@gmail.com>
2017-12-22 11:11:05 +0330

    Signal + Alarm + Timer :muscle:.

--- Go/go-c/go-c.go
+++ Go/go-c/go-c.go
@@ -19,9 +19,23 @@ import "C"
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	fmt.Println("Hello world .. from Go :D")
 	C.say_hello(C.CString("Parham Alvani"))
+	C.setup_timer()
+	C.setup_signal_handler()
+
+	go func() {
+		timer := time.Tick(100 * time.Millisecond)
+		for {
+			<-timer
+			fmt.Println("Go Go ...")
+		}
+	}()
+
+	for {
+	}
 }
--- Go/go-c/hello.h
+++ Go/go-c/hello.h
@@ -14,4 +14,6 @@
 #ifndef HELLO_H
 #define HELLO_H
 void say_hello(const char *name);
+void setup_signal_handler(void);
+void setup_timer(void);
 #endif
