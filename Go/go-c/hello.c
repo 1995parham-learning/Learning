05a040fc67fa18ad64ad4421c9a09dcd90e45607
Parham Alvani <parham.alvani@gmail.com>
2017-12-22 11:11:05 +0330

    Signal + Alarm + Timer :muscle:.
    Former-commit-id: e37a8cc88d0e317404b933f3c41ce75de819a60e

--- Go/go-c/hello.c
+++ Go/go-c/hello.c
@@ -12,7 +12,46 @@
  * Copyright (c)  2017 Parham Alvani.
  */
 #include <stdio.h>
+#include <signal.h>
+#include <sys/time.h>
 
 void say_hello(const char *name) {
 	printf("Hello %s\n", name);
+}
+
+static void signal_handler(int signo) {
+	say_hello("SIGALRM");
+}
+
+void setup_signal_handler() {
+	struct sigaction sig;
+
+	sig.sa_handler = signal_handler;
+	sig.sa_flags   = SA_ONSTACK;
+	sigemptyset(&sig.sa_mask);
+	int retval = sigaction(SIGALRM, &sig, NULL);
+
+	if(retval != 0)
+	{
+		fputs("set signal failed\n", stderr);
+		return;
+	}
+	printf("SIGALRM is ready\n");
+}
+
+void setup_timer() {
+	/* set time interval */
+	struct itimerval timer;
+	timer.it_value.tv_sec = 10;
+	timer.it_value.tv_usec = 0;
+	timer.it_interval = timer.it_value;
+
+	int retval = setitimer(ITIMER_REAL, &timer, NULL);
+
+	if(retval != 0) {
+		fputs("set timer failed\n", stderr);
+		return;
+	}
+
+	printf("timer is ready\n");
 }
