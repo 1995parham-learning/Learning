e7a5035d6bd9557cf127af35de323baa02ae3b78
Parham Alvani <parham.alvani@gmail.com>
2018-06-17 02:54:12 +0430

    Changes Ahmad guess format.
    Former-commit-id: d4632c26af1eab1b97bb28fac7cafaaaa1445413

--- C/AhmadGuess/AhmadGuess.c
+++ C/AhmadGuess/AhmadGuess.c
@@ -7,24 +7,23 @@
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
-*/
+ */
 /*
  * Copyright (c) 2015 Parham Alvani.
-*/
+ */
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-	int i = 0;
-	double sum1 = 0.0;
-	double sum2 = 0.0;
+int main(int argc, char *argv[]) {
+  int i = 0;
+  double sum1 = 0.0;
+  double sum2 = 0.0;
 
-	for (i = 1; i < 1024; i++) {
-		sum1 += 1.0 / i;
-	}
-	printf("%g\n", sum1);
-	for (i = 1024; i > 0; i--) {
-		sum2 += 1.0 / i;
-	}
-	printf("%g\n", sum2);
+  for (i = 1; i < 1024; i++) {
+    sum1 += 1.0 / i;
+  }
+  printf("%g\n", sum1);
+  for (i = 1024; i > 0; i--) {
+    sum2 += 1.0 / i;
+  }
+  printf("%g\n", sum2);
 }
