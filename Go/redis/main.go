9e313e673665b5bf51f38aa576a5584952e4a9b4
Parham Alvani <parham.alvani@gmail.com>
2018-07-11 00:43:50 +0430

    Stores data using msgpack.
    Former-commit-id: ffeddcbb8fa3bff6c264f163bc51c24c52f370fc

--- Go/redis/main.go
+++ Go/redis/main.go
@@ -14,10 +14,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 
 	"github.com/go-redis/redis"
+	"github.com/vmihailenco/msgpack"
 )
 
 type student struct {
@@ -35,13 +35,13 @@ func main() {
 	pong, err := client.Ping().Result()
 	fmt.Println(pong, err)
 
-	s1, _ := json.Marshal(student{
+	s1, _ := msgpack.Marshal(student{
 		Name:   "Parham",
 		Family: "Alvani",
 	})
 	fmt.Println(client.RPush("cache", s1).Result())
 
-	s2, _ := json.Marshal(student{
+	s2, _ := msgpack.Marshal(student{
 		Name:   "Navid",
 		Family: "Mashayekhi",
 	})
@@ -50,6 +50,6 @@ func main() {
 
 	var s3 student
 	result, err := client.RPop("cache").Bytes()
-	json.Unmarshal(result, &s3)
+	msgpack.Unmarshal(result, &s3)
 	fmt.Println(s3)
 }
