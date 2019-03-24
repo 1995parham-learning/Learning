b6c043107f361b390d908737d9c7bc5fbb27170c
Parham Alvani <parham.alvani@gmail.com>
2019-03-24 20:48:22 +0430

    Corrects lint issues of go-rpc project.
    Former-commit-id: 7cdc5e2325c7c467f7d1b9c978683aea95666c66

--- Go/go-rpc/main.go
+++ Go/go-rpc/main.go
@@ -1,15 +1,15 @@
 /*
- * In The Name Of God
- * ========================================
- * [] File Name : main.go
- *
- * [] Creation Date : 14-11-2015
- *
- * [] Created By : Parham Alvani (parham.alvani@gmail.com)
- * =======================================
+* In The Name Of God
+* ========================================
+* [] File Name : main.go
+*
+* [] Creation Date : 14-11-2015
+*
+* [] Created By : Parham Alvani (parham.alvani@gmail.com)
+* =======================================
  */
 /*
- * Copyright (c) 2015 Parham Alvani.
+* Copyright (c) 2015 Parham Alvani.
  */
 
 package main
@@ -23,21 +23,26 @@ import (
 	"net/rpc"
 )
 
+// Args contains arithmatic parameters.
 type Args struct {
 	A, B int
 }
 
+// Quotient contains division reminder and quotient
 type Quotient struct {
 	Quo, Rem int
 }
 
+// Arith is an integer with multiply and divide methods.
 type Arith int
 
+// Multiply multiplies given parameters and stores it result
 func (t *Arith) Multiply(args *Args, reply *int) error {
 	*reply = args.A * args.B
 	return nil
 }
 
+// Divide divides given parameters and stores it reminder and quotient.
 func (t *Arith) Divide(args *Args, reply *Quotient) error {
 	if args.B == 0 {
 		return errors.New("divide by zero")
@@ -48,31 +53,35 @@ func (t *Arith) Divide(args *Args, reply *Quotient) error {
 }
 
 func main() {
-	/*
-	 * Server side
-	 * ===========
-	 */
+	// Server side
+	// ===========
 
 	/* This new syntax is gooooooood :) */
 	arith := new(Arith)
 
 	/* Register arith with its associated functions */
-	rpc.Register(arith)
-	rpc.HandleHTTP()
-
-	l, e := net.Listen("tcp", ":1373")
-	if e != nil {
-		log.Fatal("listen error:", e)
+	if err := rpc.Register(arith); err != nil {
+		log.Fatalf("rpc register error: %s", err)
 	}
 
-	go http.Serve(l, nil)
+	/* Register http handler on http instance */
+	rpc.HandleHTTP()
 
-	/*
-	 * Client side
-	 * ===========
-	 */
+	l, err := net.Listen("tcp", "127.0.0.1:1373")
+	if err != nil {
+		log.Fatalf("listen error: %s", err)
+	}
 
-	client, err := rpc.DialHTTP("tcp", "localhost"+":1373")
+	go func() {
+		if err := http.Serve(l, nil); err != nil {
+			log.Fatalf("http serve error: %s", err)
+		}
+	}()
+
+	// Client side
+	// ===========
+
+	client, err := rpc.DialHTTP("tcp", "localhost:1373")
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
