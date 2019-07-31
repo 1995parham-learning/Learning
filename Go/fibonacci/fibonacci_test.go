324231a9d11d84189a7e1c0c21a941c27b8be3d4
Parham Alvani <parham.alvani@gmail.com>
2019-07-31 15:00:00 +0430

    fix: Use Go Module for Fibonacci
    Former-commit-id: e7881f017d15329a2847730f0f1493b39478c527

--- Go/fibonacci/fibonacci_test.go
+++ Go/fibonacci/fibonacci_test.go
@@ -11,7 +11,6 @@
 /*
  * Copyright (c) 2015 Parham Alvani.
  */
-
 package fibonacci
 
 import (
@@ -19,19 +18,13 @@ import (
 )
 
 func TestFibonacci(t *testing.T) {
-	var tests = []struct {
-		input int
-		want  int
-	}{
-		{2, 2},
-		{3, 3},
-		{4, 5},
-		{5, 8},
-		{6, 13},
+	if Fibonacci(2) != 2 {
+		t.Errorf("Fibonacci(2) ==> %d != 2)", Fibonacci(2))
 	}
-	for _, test := range tests {
-		if got := Fibonacci(test.input); got != test.want {
-			t.Errorf("Fibonacci(%d) ==> %d != %d)", test.input, got, test.want)
-		}
+	if Fibonacci(3) != 3 {
+		t.Errorf("Fibonacci(3) ==> %d != 3)", Fibonacci(3))
+	}
+	if Fibonacci(4) != 5 {
+		t.Errorf("Fibonacci(4) ==> %d != 5)", Fibonacci(4))
 	}
 }
