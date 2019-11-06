e9394e2d8564cf38d885626264e0f656334039c5
Parham Alvani <parham.alvani@gmail.com>
2019-11-07 00:35:43 +0330

    feat: Add Macro Based Linked-List :see_no_evil:
    Former-commit-id: fe55e9c7dcd90e7c342d3f83195d972fe3c8fcc1

---
+++ C/linked-list/main.c
@@ -0,0 +1,44 @@
+/*
+ * In The Name of God
+ * =======================================
+ * [] File Name : main.c
+ *
+ * [] Creation Date : 07-11-2019
+ *
+ * [] Created By : Parham Alvani <parham.alvani@gmail.com>
+ * =======================================
+*/
+/*
+* Copyright (c)  2019 Parham Alvani.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "linked_list.h"
+
+struct node {
+  char name[200];
+  int age;
+
+  struct node *next;
+};
+
+int main() {
+  struct node *list = malloc(sizeof(struct node));
+  list->next = NULL;
+
+  struct node *e1 = malloc(sizeof(struct node));
+  strcpy(e1->name, "Parham Alvnai");
+  e1->age = 26;
+  linked_list_append(list, e1, struct node, next);
+
+  struct node *e2 = malloc(sizeof(struct node));
+  strcpy(e2->name, "Saman Fekri");
+  e2->age = 26;
+  linked_list_append(list, e2, struct node, next);
+
+  linked_list_foreach(list, e, struct node, next) {
+    printf("%s %d\n", e->name, e->age);
+  }
+}
