b8919c83a4a25de4c15d69d62003c0b123480276
Parham Alvani <parham.alvani@gmail.com>
2020-01-26 22:28:05 +0330

    feat: Complete Logistic Regression Implementation
    Former-commit-id: 831c81060b8a7ff4e85e9b23eae7c46dda1b77a5

--- ML/fun-scikit/lr.py
+++ ML/fun-scikit/lr.py
@@ -1,35 +1,60 @@
 import numpy as np
+from typing import List
+
 
 class LogisticRegressionGD:
-    def __init__(self, eta: float, n_iter: int, random_state: int = 42):
+    '''
+    Logistic Regression Classifier using gradient descent
+    '''
+
+    def __init__(self, eta: float = 0.05, n_iter: int = 50, random_state: int = 42):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
         self._w = []
+        self._const: List[float] = []
 
     def activation(self, X):
+        '''
+        Compute logistic sigmoid activation
+        '''
         return 1 / (1 + np.exp(X))
 
     def net_input(self, X):
+        '''
+        Calculate the net input
+        '''
         return np.dot(X, self._w[1:]) + self._w[0]
 
     def fit(self, X, y):
+        '''
+        Fit training data.
+
+        Parameters
+        ----------
+        X: {array-like}, shape = [n_examples, n_features]
+        Training vectors, where n_examples is the number of
+        examples and n_features is the number of features.
+        Y: array-like, shape = [n_examples]
+        Target values.
+
+        '''
         rgen = np.random.RandomState(self.random_state)
-        self._w = rgen.normal(loc=0.0, scale=1.0,
+        self._w = rgen.normal(loc=0.0, scale=0.01,
                               size=1 + X.shape[1])
         for _ in range(self.n_iter):
             output = self.activation(self.net_input(X))
             errors = output - y
             self._w[1:] += self.eta * X.T.dot(errors)
             self._w[0] = self.eta * errors.sum()
+
+            cost = (-y.dot(np.log(output)) - ((1 - y).dot(np.log(1 - output))))
+            self._cost.append(cost)
+
         return self
 
     def predict(self, X):
-        return np.where(self.net_input(X) > 0, 1, 0)
-
-
-if __name__ == '__main__':
-    X = np.array([[1, 2], [1, 3], [1, 4]])
-    y = np.array([1, 1, 0])
-
-    print(LogisticRegressionGD(0.1, 10).fit(X, y).predict([[1, 1], [1, 5], [-1, -1], [10, 10]]))
+        '''
+        Return class label after unit step
+        '''
+        return np.where(self.net_input(X) >= 0.0, 1, 0)
