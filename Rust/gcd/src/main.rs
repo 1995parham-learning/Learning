fb3d24928ab907c0dbbafd1e0433904933b82bb0
Parham Alvani <parham.alvani@gmail.com>
2020-03-19 00:14:22 +0330

    feat: Add GCD Example Using Rust
    Former-commit-id: c4ac60d3a2d25cce469043553d02454664d95e3f

---
+++ Rust/gcd/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("gcd(10, 2) = {}", gcd(10, 2));
+    println!("gcd(2, 10) = {}", gcd(10, 2));
+    println!("gcd(15, 10) = {}", gcd(10, 2));
+}
+
+fn gcd(a: u32, b: u32) -> u32 {
+    if a % b == 0 {
+        return b;
+    }
+    return gcd(b, a % b);
+}
