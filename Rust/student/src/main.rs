009cbc2bcb6119d9fb2d1294653dbcc27c105455
Parham Alvani <parham.alvani@gmail.com>
2020-03-23 15:17:08 +0430

    feat: Add Formatter for Student
    Former-commit-id: 81fc405df6351dd46be38aa6826334f02fa7d02d

--- Rust/student/src/main.rs
+++ Rust/student/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 struct Student {
     name: String,
     id: String,
@@ -10,8 +12,14 @@ impl Student {
     }
 }
 
+impl fmt::Display for Student {
+    fn fmt(self: &Self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "{} - {} - {}", self.name, self.id, self.age)
+    }
+}
+
 fn main() {
     let s1 = Student::new(String::from("Raha Dastan"), String::from("9631025"), 20);
 
-    println!("Hello {} - {} - {}", s1.name, s1.id, s1.age);
+    println!("Hello {}", s1);
 }
