abebec5b94816dc2f8c5e4028cfc45809f6877cd
Parham Alvani <parham.alvani@gmail.com>
2020-06-28 08:39:35 +0430

    fix: Correct Lint Issues
    Former-commit-id: 43a5072e9e88fb53a6d44f2678efb588180f40e1

--- go/string/string.go
+++ go/string/string.go
@@ -10,17 +10,22 @@
 
 package main
 
-import "fmt"
-import "unicode/utf8"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	str := "ماهرپ"
 	runes := make([]byte, 0, 4)
+
 	for i := 0; i < len(str); i++ {
 		runes = append(runes, str[i])
 		if utf8.FullRune(runes) {
 			char, _ := utf8.DecodeRune(runes)
+
 			fmt.Printf("%c", char)
+
 			runes = runes[0:0]
 		}
 	}
