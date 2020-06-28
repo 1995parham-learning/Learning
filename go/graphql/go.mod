b2326a4f579b2e54f715c85f5441268857b415b5
Parham Alvani <parham.alvani@gmail.com>
2020-06-28 08:24:46 +0430

    fix: Correct Lint Issues
    Former-commit-id: d2ebcb49205d0e1a092f1c0a01eae713a80d0b59

--- go/graphql/go.mod
+++ go/graphql/go.mod
@@ -2,4 +2,7 @@ module github.com/1995parham/Learning/Go/graphql
 
 go 1.12
 
-require github.com/graphql-go/graphql v0.7.7
+require (
+	github.com/graphql-go/graphql v0.7.7
+	github.com/kr/pretty v0.2.0 // indirect
+)
--- go/graphql/main.go
+++ go/graphql/main.go
@@ -20,6 +20,10 @@ import (
 	"github.com/graphql-go/graphql"
 )
 
+const (
+	diceFaces = 6
+)
+
 func main() {
 	rand.Seed(time.Now().Unix())
 
@@ -47,13 +51,14 @@ func main() {
 				}
 
 				for i := 0; i < n; i++ {
-					l = append(l, rand.Intn(6)+1)
+					l = append(l, rand.Intn(diceFaces)+1)
 				}
 				return l, nil
 			},
 		},
 	}
 	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+
 	schema, err := graphql.NewSchema(graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)})
 	if err != nil {
 		log.Fatalf("failed to create new schema, error: %v", err)
@@ -66,10 +71,16 @@ func main() {
 	}
 	`
 	r := graphql.Do(graphql.Params{Schema: schema, RequestString: query})
+
 	if len(r.Errors) > 0 {
 		log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
 	}
-	rj, _ := json.Marshal(r)
-	fmt.Printf("client request: %s \n", query)
-	fmt.Printf("server answer: %s \n", rj)
+
+	rj, err := json.Marshal(r)
+	if err != nil {
+		log.Fatalf("json marshal failed %s", err)
+	}
+
+	fmt.Printf("client request: %s\n", query)
+	fmt.Printf("server answer: %s\n", rj)
 }
