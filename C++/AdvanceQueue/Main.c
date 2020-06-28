ba7999e7c1bdbfd9626f092e757a4ac449a185a6
Parham Alvani <parham.alvani@gmail.com>
2020-06-28 09:58:44 +0430

    feat: Remove Unused Languages
    Former-commit-id: c57e603f5c2088551627d122e8bc7a331df77cb4

--- C++/AdvanceQueue/Main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "AdvanceQueue.h"
-
-int main(int argc,char* argv[]){
-	queue_t* ptr = create_queue();
-	enqueue(10,  ptr);
-	printf("Hip Hip ..... %d\n", dequeue(ptr));
-}
--- C++/Priority.c
+++
@@ -1,9 +0,0 @@
-#include <sys/time.h>
-#include <sys/resource.h>
-#include <stdio.h>
-
-int main(int argc,char* argv[]){
-	int ret;
-	ret = getpriority(PRIO_PROCESS,0);
-	printf("nice value is %d\n", ret);
-}
