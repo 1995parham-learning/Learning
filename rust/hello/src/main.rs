32e646fae86dc39da0a084c256698baff4b8ece6
Parham Alvani <parham.alvani@gmail.com>
2020-07-17 10:54:46 +0430

    feat: Add From Data

--- rust/hello/src/main.rs
+++ rust/hello/src/main.rs
@@ -1,16 +1,34 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
+
+struct From {
+    from: String
+}
+
+impl From {
+    pub fn new(from: &str) -> Self {
+        From {
+            from: String::from(from),
+        }
+    }
+
+    pub fn from(&self) -> &str {
+        self.from.as_str()
+    }
+}
 
 #[get("/hello")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().json("Hi Elahe ✋")
+async fn hello(data: web::Data<From>) -> impl Responder {
+    HttpResponse::Ok().json(
+        format!("Hi Elahe ✋ from {}", data.from())
+        )
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(hello)
+        App::new().data(From::new("Parham")).service(hello)
     })
-    .bind("127.0.0.1:8088")?
+    .bind("127.0.0.1:1378")?
     .run()
     .await
 }
