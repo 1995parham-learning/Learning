187f46da9da883b52a89bf64d6a7e11bf4f34f0a
Parham Alvani <parham.alvani@gmail.com>
2020-07-18 02:25:22 +0430

    feat: Use Concurrent Env in Gurobi

--- gurobi/brewery.py
+++ gurobi/brewery.py
@@ -6,6 +6,10 @@ A = m.addVar(vtype=GRB.INTEGER, name="Ale")
 B = m.addVar(vtype=GRB.INTEGER, name="Beer")
 m.update()
 
+# two concurrent instances
+m.getConcurrentEnv(0)
+m.getConcurrentEnv(1)
+
 c1 = m.addConstr(4 * A + 4 * B <= 160)
 c2 = m.addConstr(35 * A + 20 * B <= 1190)
 c3 = m.addConstr(5 * A + 15 * B <= 480)
