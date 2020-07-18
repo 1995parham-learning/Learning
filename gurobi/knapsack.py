3fd838a413234bfd942e157a06feea4c64d87abb
Parham Alvani <parham.alvani@gmail.com>
2020-07-18 09:35:40 +0430

    feat: Improve Knapsack Model

--- gurobi/knapsack.py
+++ gurobi/knapsack.py
@@ -1,14 +1,34 @@
-from gurobipy import Model, GRB
+from gurobipy import Model, GRB, quicksum
+
+import typing
+import dataclasses
+
+
+@dataclasses.dataclass
+class Item:
+    weight: int
+    profit: int
+
+
+items: typing.List[Item] = [Item(1, 10), Item(1, 1)]
+
+capacity = 10
+
 
 m = Model("knapsack")
 
-b1 = m.addVar(vtype=GRB.BINARY)
-b2 = m.addVar(vtype=GRB.BINARY)
-b3 = m.addVar(vtype=GRB.BINARY)
-b4 = m.addVar(vtype=GRB.BINARY)
+b = m.addVars(len(items), name="item", vtype=GRB.BINARY)
+m.update()
 
-m.addConstr(10 * b1 + 5 * b2 + 10 * b3 + 11 * b4 <= 23)
-m.setObjective(b1 + b2 + b3 + b4, GRB.MAXIMIZE)
+
+m.addConstr(
+    quicksum(item.weight * b[i] for (i, item) in enumerate(items)) <= capacity,
+    name="capacity",
+)
+m.setObjective(
+    quicksum(b[i] * item.profit for (i, item) in enumerate(items)),
+    GRB.MAXIMIZE,
+)
 m.update()
 
 print(m)
