49981cffd9b115d218cfc49a4026a4d83defcc17
Parham Alvani <parham.alvani@gmail.com>
2020-08-10 13:22:51 +0430

    feat: Better Minigrep

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,34 +1,16 @@
-use std::{env, fs, process};
+use minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("problem parsing arguments: {}", err);
+        eprintln!("problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    let content = fs::read_to_string(config.filename).unwrap_or_else(|err| {
-        println!("failed to read: {}", err);
-        process::exit(1);
-    });
-    println!("{}", content);
-}
-
-struct Config {
-    filename: String,
-    query: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("minigrep <query> <filename>");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+    if let Err(err) = minigrep::run(config) {
+        eprintln!("application error: {}", err);
+        process::exit(1)
     }
 }
