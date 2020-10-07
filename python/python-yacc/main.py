4c3b7b4cb3bff05cd0f7cd370c53c4f80894198d
Parham Alvani <parham.alvani@gmail.com>
2020-10-08 03:27:21 +0330

    feat: Improve Python Compiler Example

--- python/python-yacc/main.py
+++ python/python-yacc/main.py
@@ -10,16 +10,17 @@ import ply.lex as lex
 import ply.yacc as yacc
 
 tokens = (
-    'NAME', 'NUMBER',
+    "NAME",
+    "NUMBER",
 )
 
-literals = ['=', '+', '-', '*', '/', '(', ')', ';']
+literals = ["=", "+", "-", "*", "/", "(", ")", ";"]
 
-t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_NAME = r"[a-zA-Z_][a-zA-Z0-9_]*"
 
 
 def t_NUMBER(t):
-    r'\d+'
+    r"\d+"
     try:
         t.value = int(t.value)
     except ValueError:
@@ -33,7 +34,7 @@ t_ignore = " \t"
 
 
 def t_newline(t):
-    r'\n+'
+    r"\n+"
     t.lexer.lineno += t.value.count("\n")
 
 
@@ -48,56 +49,59 @@ lexer = lex.lex()
 # Parsing rules
 
 precedence = (
-        ('left', '+', '-'),
-        ('left', '*', '/'),
-        ('right', 'UMINUS'),
+    ("left", "+", "-"),
+    ("left", "*", "/"),
+    ("right", "UMINUS"),
 )
 
 # dictionary of names
 names = {}
 
 # temporary variables
-inc = 0
+IDX = 0
 
 
 def p_program(p):
-    '''
+    """
     program : program statement
             | statement
-    '''
+    """
     pass
 
 
 def p_statement_assign(p):
     'statement : NAME "=" expression ";"'
-    print('%s = MOV %s' % (p[1], p[3]))
+    print("%s = MOV %s" % (p[1], p[3]))
     names[p[1]] = p[3]
 
 
 def p_expression_binop(p):
-    '''expression : expression '+' expression
-                  | expression '-' expression
-                  | expression '*' expression
-                  | expression '/' expression'''
-    global inc
-    p[0] = 't%d' % inc
-    inc = inc + 1
-    if p[2] == '+':
-        print('%s = ADD %s %s' % (p[0], p[1], p[3]))
-    elif p[2] == '-':
-        print('%s = MINUS %s %s' % (p[0], p[1], p[3]))
-    elif p[2] == '*':
-        print('%s = MULT %s %s' % (p[0], p[1], p[3]))
-    elif p[2] == '/':
-        print('%s = DIV %s %s' % (p[0], p[1], p[3]))
+    """expression : expression '+' expression
+    | expression '-' expression
+    | expression '*' expression
+    | expression '/' expression"""
+
+    global IDX
+    p[0] = f"t{IDX}"
+    IDX += 1
+
+    if p[2] == "+":
+        print("%s = ADD %s %s" % (p[0], p[1], p[3]))
+    elif p[2] == "-":
+        print("%s = MINUS %s %s" % (p[0], p[1], p[3]))
+    elif p[2] == "*":
+        print("%s = MULT %s %s" % (p[0], p[1], p[3]))
+    elif p[2] == "/":
+        print("%s = DIV %s %s" % (p[0], p[1], p[3]))
 
 
 def p_expression_uminus(p):
     "expression : '-' expression %prec UMINUS"
-    global inc
-    p[0] = 't%d' % inc
-    inc = inc + 1
-    print('%s = MINUS 0 %s' % (p[0], p[2]))
+    global IDX
+    p[0] = f"t{IDX}"
+    IDX = IDX + 1
+
+    print("%s = MINUS 0 %s" % (p[0], p[2]))
 
 
 def p_expression_group(p):
@@ -125,11 +129,11 @@ def p_error(p):
 
 
 # Test it out
-data = '''
+DATA = """
 A = 1 + 3 * 4;
 B = A + 1;
-'''
+"""
 
 # Give the ply some input
 yacc.yacc()
-yacc.parse(data)
+yacc.parse(DATA)
