c915d88bea62930dd7b23d07c79e62acb9e91a42
Parham Alvani <parham.alvani@gmail.com>
2020-12-03 14:53:01 +0330

    fix: This Should Work

--- go/fibonacci/fibonacci_test.go
+++ go/fibonacci/fibonacci_test.go
@@ -15,7 +15,6 @@ package fibonacci_test
 
 import (
 	"fibonacci"
-	"fmt"
 	"testing"
 )
 
@@ -23,18 +22,22 @@ func TestFibonacci(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
+		name     string
 		input    int
 		expected int
 	}{
 		{
+			name:     "2",
 			input:    2,
 			expected: 2,
 		},
 		{
+			name:     "3",
 			input:    3,
 			expected: 3,
 		},
 		{
+			name:     "4",
 			input:    4,
 			expected: 5,
 		},
@@ -42,7 +45,7 @@ func TestFibonacci(t *testing.T) {
 
 	for _, tc := range tests {
 		tc := tc
-		t.Run(fmt.Sprintf("%d", tc.input), func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 			if fibonacci.Fibonacci(tc.input) != tc.expected {
 				t.Errorf("Fibonacci(%d) ==> %d != %d)", tc.input, fibonacci.Fibonacci(tc.input), tc.expected)
