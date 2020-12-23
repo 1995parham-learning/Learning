d36b79d89e8f7e49d995a571dd8da016c543e049
Parham Alvani <parham.alvani@gmail.com>
2020-12-23 22:39:25 +0330

    feat: Improve Iter Example

--- rust/iter/src/main.rs
+++ rust/iter/src/main.rs
@@ -1,4 +1,27 @@
 use std::collections::HashMap;
+use std::iter::Iterator;
+
+struct Counter {
+    count: i32,
+}
+
+impl Counter {
+    fn new() -> Self {
+        Self { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count == 5 {
+            return None;
+        }
+        self.count += 1;
+        Some(self.count)
+    }
+}
 
 fn main() {
     let a = vec![1, 2, 3];
@@ -16,4 +39,7 @@ fn main() {
     name.chars()
         .map(|b| (b, 1))
         .for_each(|i| println!("({}, {})", i.0, i.1));
+
+    let counter = Counter::new();
+    println!("{}", counter.sum::<i32>());
 }
