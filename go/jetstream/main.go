8cbe3e1c555123df16acdd4936a62a55978dddf9
Parham Alvani <parham.alvani@gmail.com>
2021-01-22 16:49:58 +0330

    feat: Complete Jetstream Example

--- go/jetstream/main.go
+++ go/jetstream/main.go
@@ -16,24 +16,58 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/nats-io/jsm.go"
 	"github.com/nats-io/nats.go"
 )
 
-const server = "nats://127.0.0.1:4222"
+const (
+	ConsumerName      = "elconsumer"
+	NATSServer        = "nats://127.0.0.1:4222"
+	ConnectionTimeout = 10 * time.Second
+)
 
 func main() {
-	nc, err := nats.Connect(server)
+	nc, err := nats.Connect(NATSServer)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer nc.Close()
 
 	fmt.Printf("Connected to %s from %v\n", nc.ConnectedAddr(), nc.DiscoveredServers())
 
-	c, err := nats.NewEncodedConn(nc, nats.GOB_ENCODER)
+	mgr, err := jsm.New(nc, jsm.WithTimeout(ConnectionTimeout))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer c.Close()
+	stream, err := mgr.LoadOrNewStream("ORDERS",
+		jsm.Subjects("ORDERS.*"),
+		jsm.MaxAge(24*365*time.Hour),
+		jsm.MemoryStorage(),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	consumer, err := stream.LoadOrNewConsumer(
+		ConsumerName,
+		jsm.FilterStreamBySubject("ORDERS.new"),
+		jsm.DurableName(ConsumerName),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := nc.Publish("ORDERS.new", []byte("hello world")); err != nil {
+		log.Fatal(err)
+	}
+
+	msg, err := consumer.NextMsg()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(msg.Data))
 }
