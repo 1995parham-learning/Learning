acb840f44eb7ef67e45454920032b8b9cc750a06
Parham Alvani <parham.alvani@gmail.com>
2021-01-30 22:30:59 +0330

    feat: Add Comments

--- go/sentry/main.go
+++ go/sentry/main.go
@@ -12,16 +12,19 @@ var ErrChild = eris.New("i am child")
 
 const SentryTimeout = 10 * time.Second
 
+// child returns child error.
 func child() error {
 	return ErrChild
 }
 
+// parent wraps child error and returns it.
 func parent() error {
 	return eris.Wrap(child(), "i am parent")
 }
 
 func main() {
 	if err := sentry.Init(sentry.ClientOptions{
+		// Use the dsn from your sentry
 		Dsn:         "sentry-url",
 		Debug:       true,
 		Environment: "Development",
