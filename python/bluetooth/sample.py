7249c4619c47d2be6dac408ccb882df8012ac070
Parham Alvani <parham.alvani@gmail.com>
2021-04-18 16:25:40 +0430

    fix: Correct Intention

--- python/bluetooth/sample.py
+++ python/bluetooth/sample.py
@@ -12,4 +12,4 @@ nearby_devices = bluetooth.discover_devices(lookup_names=True)
 print("found %d devices" % len(nearby_devices))
 
 for addr, name in nearby_devices:
-        print("*  %s - %s" % (addr, name))
+    print("*  %s - %s" % (addr, name))
