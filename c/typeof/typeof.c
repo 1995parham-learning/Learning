812afc6c735012ca046140303d9d7880eddceaac
Parham Alvani <parham.alvani@gmail.com>
2021-05-24 07:41:41 +0430

    feat: Update typeof Example

--- c/typeof/typeof.c
+++ c/typeof/typeof.c
@@ -10,8 +10,13 @@
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
  */
+
+
 int main(int argc, char *argv[]) {
   int A = 10;
+	// since typeof is a compiler extension, there is not really a definition for it,
+	// but in the tradition of C it would be an operator,
+	// e.g sizeof and _Alignof are also seen as an operators.
   typeof(A) *a;
   a = &A;
 }
