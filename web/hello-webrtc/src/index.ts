f4f505ed5bf0665d4a58fc0d44d8f5d0367d591f
Parham Alvani <parham.alvani@gmail.com>
2021-06-12 03:03:23 +0430

    feat: Update Webrtc Example

--- web/hello-webrtc/src/index.ts
+++ web/hello-webrtc/src/index.ts
@@ -1,4 +1,6 @@
 async function onInit() {
+  await loadDevices();
+
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       video: true,
@@ -8,6 +10,18 @@ async function onInit() {
     el.srcObject = stream;
   } catch (err) {
     console.log(`cannot get the user media: ${err}`);
+  }
+}
+
+async function loadDevices() {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const el = document.getElementById("devices");
+
+  for (const device of devices) {
+    const li = document.createElement("li");
+    li.innerHTML = `<i>${device.label}</i> [${device.deviceId}]`;
+
+    el.appendChild(li);
   }
 }
 
