11c5b617fb127524259bb117e7c0c15f554425a3
Parham Alvani <parham.alvani@gmail.com>
2021-06-18 08:08:49 +0430

    feat: Learning Pytorch

--- ml/hello-torch/intro.py
+++ ml/hello-torch/intro.py
@@ -8,3 +8,14 @@ print(a.flatten())
 print(a.flatten(start_dim=1))
 
 print(a.view(2, -1))
+
+print("\n===\n")
+
+b = torch.ones(2, 2)
+print(b)
+c = torch.ones(2, 2)
+print(c)
+
+r = b @ c
+print(r)
+print(r.shape)
