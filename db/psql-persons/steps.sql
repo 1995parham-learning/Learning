2a2ee4b8c7cbb424b993ca622bb5822b662284c6
Parham Alvani <parham.alvani@gmail.com>
2021-10-10 02:07:56 +0330

    feat: add persons example

---
+++ db/psql-persons/steps.sql
@@ -0,0 +1,12 @@
+-- create a simple table for storing first_name and last_name of persons.
+create table if not exists persons (
+  first_name text,
+  last_name text
+);
+
+insert into persons values ('Elahe', 'Dastan');
+insert into persons values ('Sara', 'Dastan');
+
+alter table if exists persons add column if not exists id text;
+
+insert into persons values ('Negin', 'Amjadi', '0017784646');
