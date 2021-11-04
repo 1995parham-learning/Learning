2bdc49f6eb32fb36567f177e3e785ef5f664dbd8
Parham Alvani <parham.alvani@gmail.com>
2021-11-04 16:57:40 +0330

    feat: update data-source script

--- db/psql-movie/data-source.sh
+++ db/psql-movie/data-source.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
+
+echo 'download dvd rental database sample'
 
 wget https://www.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip
 unzip dvdrental.zip
+rm dvdrental.zip
