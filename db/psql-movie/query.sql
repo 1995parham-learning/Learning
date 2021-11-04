b093386d4635e9bb20ee30f75c84e6af603636a4
Parham Alvani <parham.alvani@gmail.com>
2021-11-04 18:34:00 +0330

    feat: update queries

--- db/psql-movie/query.sql
+++ db/psql-movie/query.sql
@@ -1,14 +1,16 @@
 create extension if not exists tablefunc;
 
-select store.store_id, category.name from store, rental, film, inventory, staff, film_category, category where
+select store.store_id, category.name, count(film.film_id) from store, rental, film, inventory, staff, film_category, category where
   store.store_id = staff.store_id and rental.staff_id = staff.staff_id and
   rental.inventory_id = inventory.inventory_id and inventory.film_id = film_category.film_id and
-  film_category.film_id = category.category_id;
+  film_category.film_id = category.category_id
+  group by store.store_id, category.name order by store_id;
 
 select * from crosstab(
-  'select store.store_id, category.name, category.id from store, rental, film, inventory, staff, film_category, category where
+  'select store.store_id, category.name, count(film.film_id) from store, rental, film, inventory, staff, film_category, category where
   store.store_id = staff.store_id and rental.staff_id = staff.staff_id and
   rental.inventory_id = inventory.inventory_id and inventory.film_id = film_category.film_id and
-  film_category.film_id = category.category_id',
+  film_category.film_id = category.category_id
+  group by store.store_id, category.name order by store_id',
   'select "1"'
 ) as categories(one int);
