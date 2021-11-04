d5be0becd70232796bc9646e8b44793d83842cd0
Parham Alvani <parham.alvani@gmail.com>
2021-11-04 19:00:46 +0330

    feat: working query

--- db/psql-movie/query.sql
+++ db/psql-movie/query.sql
@@ -7,10 +7,10 @@ select store.store_id, category.name, count(film.film_id) from store, rental, fi
   group by store.store_id, category.name order by store_id;
 
 select * from crosstab(
-  'select store.store_id, category.name, count(film.film_id) from store, rental, film, inventory, staff, film_category, category where
+  'select store.store_id, category.category_id, count(film.film_id) from store, rental, film, inventory, staff, film_category, category where
   store.store_id = staff.store_id and rental.staff_id = staff.staff_id and
   rental.inventory_id = inventory.inventory_id and inventory.film_id = film_category.film_id and
   film_category.film_id = category.category_id
-  group by store.store_id, category.name order by store_id',
-  'select "1"'
-) as categories(one int);
+  group by store.store_id, category.category_id order by store_id',
+  'select category_id from category limit 2'
+) as (store int, cat1 int, cat2 int);
