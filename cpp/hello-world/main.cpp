0b3a821f6ccd613fdc57b1acc83d354bd158945d
Parham Alvani <parham.alvani@gmail.com>
2022-02-23 20:53:34 +0330

    feat: update main.cpp

--- cpp/hello-world/main.cpp
+++ cpp/hello-world/main.cpp
@@ -2,6 +2,6 @@
 #include <fmt/core.h>
 
 int main() {
-  std::cout << fmt::format("Hello, World {}", 10) << std::endl;
+  std::cout << fmt::format("Hello, World {}", 42) << std::endl;
   return 0;
 }
