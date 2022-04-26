a686f5f55f06f2f1811f6fab704ff063d7de0bca
Parham Alvani <parham.alvani@gmail.com>
2022-04-26 09:45:25 +0430

    feat: update java hello example

--- java/hello/app/src/main/java/hello/App.java
+++ java/hello/app/src/main/java/hello/App.java
@@ -1,27 +1,13 @@
 package hello;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class App {
-    private List<String> friends;
-
-    public App() {
-      this.friends = new ArrayList<>();
-      this.friends.addAll(List.of("Navid", "Mohammad"));
-    }
-
-    public List<String> getFriends() {
-      return this.friends;
-    }
-
-    public void addFriends(String name) {
-      this.friends.add(name);
-    }
-
     public static void main(String[] args) {
-      var app = new App();
+      Person navid = new Person("Navid", "Mashayekhi");
+      Person mohammad = new Person("Mohammad", "Mahboubi");
+      Person parham = new Person("Parham", "Alvani");
+      parham.addFriends(navid);
+      parham.addFriends(mohammad);
 
-      System.out.println(app.getFriends());
+      System.out.println(parham);
     }
 }
