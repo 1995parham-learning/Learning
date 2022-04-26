a5350c2af2aee9bb00e56a648fc0578d27a0cd65
Parham Alvani <parham.alvani@gmail.com>
2022-04-26 09:45:25 +0430

    feat: update java hello example

--- java/hello/app/src/test/java/hello/AppTest.java
+++ java/hello/app/src/test/java/hello/AppTest.java
@@ -1,11 +1,20 @@
 package hello;
 
 import org.junit.jupiter.api.Test;
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getFriends(), "app should have a list of my friends");
+    @Test void parhamHasFriends() {
+      Person navid = new Person("Navid", "Mashayekhi");
+      Person mohammad = new Person("Mohammad", "Mahboubi");
+      Person parham = new Person("Parham", "Alvani");
+      parham.addFriends(navid);
+      parham.addFriends(mohammad);
+
+      assertNotNull(parham.getFriends());
+      assertEquals("Parham", parham.getFirstName());
+      assertEquals("Alvani", parham.getLastName());
+      assertEquals(List.of(navid, mohammad), parham.getFriends());
     }
 }
