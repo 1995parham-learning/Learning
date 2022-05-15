af1fdaac4b5d714de5092ded90b6a74d027513a4
Parham Alvani <parham.alvani@gmail.com>
2022-05-15 15:09:13 +0430

    feat: improve emq example

--- python/emqtt/client.py
+++ python/emqtt/client.py
@@ -1,33 +1,35 @@
-# In The Name Of God
-# ========================================
-# [] File Name : client.py
-#
-# [] Creation Date : 11-10-2016
-#
-# [] Created By : Parham Alvani (parham.alvani@gmail.com)
-# =======================================
 import paho.mqtt.client as mqtt
 
-# The callback for when the client receives a CONNACK response from the server.
+
 def on_connect(client, userdata, flags, rc):
+    '''
+    The callback for when the client receives a
+    CONNACK response from the server.
+    '''
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("$SYS/#")
 
-# The callback for when a PUBLISH message is received from the server.
+
 def on_message(client, userdata, msg):
+    '''
+    The callback for when a PUBLISH message is received from the server.
+    '''
     print(msg.topic + " " + str(msg.payload))
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
 
-client.connect("127.0.0.1", 1883, 60)
+if __name__ == "__main__":
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+    client.connect("127.0.0.1", 1883, 60)
+
+    # Blocking call that processes network traffic, dispatches callbacks and
+    # handles reconnecting.
+    # Other loop*() functions are available that
+    # give a threaded interface and a
+    # manual interface.
+    client.loop_forever()
