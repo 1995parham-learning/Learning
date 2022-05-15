38e9dbd5c5acc1892c9eff0fccd440c825a84996
Parham Alvani <parham.alvani@gmail.com>
2022-05-15 16:29:56 +0430

    feat: update pyroy project

--- python/pyroy/pyroy.py
+++ python/pyroy/pyroy.py
@@ -1,15 +1,6 @@
-# In The Name Of God
-# ========================================
-# [] File Name : pyroy.py
-#
-# [] Creation Date : 10/17/15
-#
-# [] Created By : Parham Alvani (parham.alvani@gmail.com)
-# =======================================
-__author__ = 'Parham Alvani'
+import wave
 
 import pyaudio
-import wave
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -18,48 +9,50 @@ RATE = 4410
 RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "output.wav"
 
-p = pyaudio.PyAudio()
+if __name__ == "__main__":
+    p = pyaudio.PyAudio()
 
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
 
-print("* first recording")
+    print("* first recording")
 
-first_frames = []
+    first_frames = []
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    first_frames.append(data)
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        first_frames.append(data)
 
-print("* done first recording")
+    print("* done first recording")
 
-print("* second recording")
+    print("* second recording")
 
-second_frames = []
+    second_frames = []
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    second_frames.append(data)
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        second_frames.append(data)
 
-print("* done second recording")
+    print("* done second recording")
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
-print(len(first_frames))
-print(len(second_frames))
+    print(len(first_frames))
+    print(len(second_frames))
 
-frames = [y for y in first_frames]
-for x in second_frames:
-    frames.append(x)
+    frames = first_frames[:]
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+    for x in second_frames:
+        frames.append(x)
+
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
